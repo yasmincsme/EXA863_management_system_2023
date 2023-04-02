c2fd7d89df4c68ef0f7662c37e79a2664051e8bf
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-02 13:03:25 -0300

    Add a new class named "Person"

---
+++ src/main/java/com/example/exa863_management_system_2023/model/Person.java
@@ -0,0 +1,34 @@
+package com.example.exa863_management_system_2023.model;
+
+public abstract class Person {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail() {
+        this.email = email;
+    }
+}
+
