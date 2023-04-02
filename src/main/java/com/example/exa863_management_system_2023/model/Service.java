6d3072b8739946d5b813992f10f654fe3ace7519
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-02 13:29:23 -0300

    Add a new class called "Service"

---
+++ src/main/java/com/example/exa863_management_system_2023/model/Service.java
@@ -0,0 +1,28 @@
+package com.example.exa863_management_system_2023.model;
+
+public abstract class Service {
+
+    private String name;
+    private String description;
+    private Double price;
+    private Double cost;
+
+    public Service(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription() {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice() {
+        this.price = price;
+    }
+}
+
