7cb3fbfc44f94a39a7e4979daabae3068d3711bd
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-02 15:39:40 -0300

    Add a new class called "Building"

--- src/main/java/com/example/exa863_management_system_2023/model/Building.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Building.java
@@ -1,7 +1,19 @@
 package com.example.exa863_management_system_2023.model;
 
+import java.util.List;
+
 public class Building extends Service{
 
+    private List<ComputerComponent> usedComponents;
 
+    public Building(String name) {
+        super(name);
+    }
 
+    public List<ComputerComponent> getUsedComponents() {
+        return usedComponents;
+    }
+    public void setUsedComponents() {
+        this.usedComponents = usedComponents;
+    }
 }
