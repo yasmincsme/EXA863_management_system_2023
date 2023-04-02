d5f83f74bc44d470f6885e335f69c66dba4c819d
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-02 13:46:46 -0300

    Add a new class called "Cleaning"

---
+++ src/main/java/com/example/exa863_management_system_2023/model/Cleaning.java
@@ -0,0 +1,19 @@
+package com.example.exa863_management_system_2023.model;
+
+import java.util.List;
+
+public class Cleaning extends Service{
+
+    private List<String> components;
+
+    public Cleaning(String name) {
+        super(name);
+    }
+
+    public List<String> getComponents() {
+        return getComponents();
+    }
+    public void setComponents() {
+        this.components = components;
+    }
+}
