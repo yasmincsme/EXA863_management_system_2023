750b474643ee7f07bee1f337784de477e0b622a4
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-02 16:50:22 -0300

    Created the class "Technician"

---
+++ src/main/java/com/example/exa863_management_system_2023/model/Technician.java
@@ -0,0 +1,17 @@
+package com.example.exa863_management_system_2023.model;
+
+public class Technician extends Person {
+
+    private WorkOrder workOrder;
+
+    public Technician(String name) {
+        super(name);
+    }
+
+    public WorkOrder getWorkorder() {
+        return workOrder;
+    }
+    public void setWorkOrder() {
+        this.workOrder = workOrder;
+    }
+}
