d0e017a9b271769d9cfe2991b950409325fe9651
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-04 17:40:18 -0300

    Created CRUD

---
+++ src/main/java/com/example/exa863_management_system_2023/dao/CRUD.java
@@ -0,0 +1,13 @@
+package com.example.exa863_management_system_2023.dao;
+
+import java.util.List;
+
+public interface CRUD<T, E extends Exception> {
+
+    public T create(T object);
+    public T findbyID(int id);
+    public List<T> findMany();
+    public void update(T object) throws E;
+    public void delete(int id) throws E;
+    public void deleteMany();
+}
--- src/main/java/com/example/exa863_management_system_2023/dao/DAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/DAO.java
@@ -1,5 +1,5 @@
 package com.example.exa863_management_system_2023.dao;
 
-public class DAO {
+public class DAO implements CRUD {
 
 }
