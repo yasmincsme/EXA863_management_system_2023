b6b0cbfeb4c95f84c85755ef03d6b787cfc3d802
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-04 23:08:51 -0300

    create the class "ClientDAO"

---
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientDAO.java
@@ -0,0 +1,10 @@
+package com.example.exa863_management_system_2023.dao.client;
+
+import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.Client;
+
+import java.util.List;
+
+public interface ClientDAO extends CRUD<Client, Exception> {
+    public List<Client> findbyID(S)
+}
