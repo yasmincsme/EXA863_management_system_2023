b203f23e8fa0a72bb4b96355ea9f2aa1bd5d5f8e
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-08 22:12:42 -0300

    Update the class "Person"

--- src/main/java/com/example/exa863_management_system_2023/model/Person.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Person.java
@@ -2,7 +2,7 @@ package com.example.exa863_management_system_2023.model;
 
 public abstract class Person {
 
-    private String id;
+    private int id;
     private String name;
     private String email;
 
@@ -10,10 +10,10 @@ public abstract class Person {
         this.name = name;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
