d5a38d0676d7d57b556f5bac0fdafa4b65d310c8
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-08 22:12:23 -0300

    Update the class "DAO"

--- src/main/java/com/example/exa863_management_system_2023/dao/DAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/DAO.java
@@ -1,5 +1,10 @@
 package com.example.exa863_management_system_2023.dao;
 
-public class DAO implements CRUD {
+import com.example.exa863_management_system_2023.dao.client.ClientDAO;
+
+public class DAO {
+
+    private static ClientDAO clientDAO;
+
 
 }
