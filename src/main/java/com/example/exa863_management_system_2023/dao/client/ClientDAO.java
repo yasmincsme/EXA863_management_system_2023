06087ba816ac705f1c7aa32dadc23f7ff2ee5cc2
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-08 22:11:33 -0300

    Update the class "ClientDAO"

--- src/main/java/com/example/exa863_management_system_2023/dao/client/ClientDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientDAO.java
@@ -6,5 +6,7 @@ import com.example.exa863_management_system_2023.model.Client;
 import java.util.List;
 
 public interface ClientDAO extends CRUD<Client, Exception> {
-    public List<Client> findbyID(S)
+
+    public List<Client> findByName(String name);
+    public Client findByID(int id);
 }
