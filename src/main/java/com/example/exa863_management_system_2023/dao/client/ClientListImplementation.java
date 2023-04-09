b10ac182ee67fd1433d36e69825a1cfec67bc337
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-08 22:11:59 -0300

    Update the class "ClientListImplementation"

--- src/main/java/com/example/exa863_management_system_2023/dao/client/ClientListImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientListImplementation.java
@@ -3,7 +3,6 @@ package com.example.exa863_management_system_2023.dao.client;
 import com.example.exa863_management_system_2023.model.Client;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ClientListImplementation implements ClientDAO {
@@ -14,6 +13,44 @@ public class ClientListImplementation implements ClientDAO {
     public ClientListImplementation() {
         this.listOfClients = new ArrayList<Client>();
         this.nextID = 0;
+    }
+
+    @Override
+    public Client create(Client client) {
+        client.setId(this.nextID);
+        this.nextID++;
+        this.listOfClients.add(client);
+        return client;
+    }
+
+    @Override
+    public List<Client> findMany() {
+        List<Client> clientList = new ArrayList<Client>();
+        for(Object client : this.listOfClients) {
+            clientList.add((Client) client);
+        }
+        return clientList;
+    }
+
+    @Override
+    public Client findByID(int id) {
+        for(Client client : this.listOfClients) {
+            if(client.getId() == id) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Client> findByName(String name) {
+        List<Client> clientList = new ArrayList<Client>();
+        for (Client client : this.listOfClients) {
+            if (client.getName() == name) {
+                clientList.add(client);
+            }
+        }
+        return clientList;
     }
 
 }
