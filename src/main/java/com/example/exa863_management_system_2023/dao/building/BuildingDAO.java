8a1532cd02b4cf800ede5112a23ec8f35780fa0f
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-09 18:52:17 -0300

    Create the interface "BuildingDAO"

---
+++ src/main/java/com/example/exa863_management_system_2023/dao/building/BuildingDAO.java
@@ -0,0 +1,7 @@
+package com.example.exa863_management_system_2023.dao.building;
+
+import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.Building;
+
+public interface BuildingDAO extends CRUD<Building, Exception> {
+}
