9aedfe7070c3fb16362b66bca889e1bcffedb927
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-09 14:08:07 -0300

    Update the interface "ComputerComponentDAO"

---
+++ src/main/java/com/example/exa863_management_system_2023/dao/computerComponent/ComputerComponentLDAO.java
@@ -0,0 +1,7 @@
+package com.example.exa863_management_system_2023.dao.computerComponent;
+
+import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.ComputerComponent;
+
+public interface ComputerComponentLDAO extends CRUD<ComputerComponent, Exception> {
+}
