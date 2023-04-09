34cfbfe6148ab5eefcadadd9cedcc0970ee47aca
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-09 14:05:20 -0300

    Update in the class "Person" - Modifying getters and setters

--- src/main/java/com/example/exa863_management_system_2023/model/Person.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Person.java
@@ -27,7 +27,7 @@ public abstract class Person {
     public String getEmail() {
         return email;
     }
-    public void setEmail() {
+    public void setEmail(String email) {
         this.email = email;
     }
 }
