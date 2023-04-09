20ed1252a637cf9ee670f83e69d0437f06452ac6
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-09 18:52:40 -0300

    Create the interface "CleaningDAO"

---
+++ src/main/java/com/example/exa863_management_system_2023/dao/cleaning/CleaningDAO.java
@@ -0,0 +1,7 @@
+package com.example.exa863_management_system_2023.dao.cleaning;
+
+import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.Cleaning;
+
+public interface CleaningDAO extends CRUD<Cleaning, Exception> {
+}
