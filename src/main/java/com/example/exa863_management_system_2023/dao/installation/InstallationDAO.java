9781fa6ba5c72918822544f32e18cb714c5d55b8
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-09 14:08:38 -0300

    Update the interface "InstallationDAO"

---
+++ src/main/java/com/example/exa863_management_system_2023/dao/installation/InstallationDAO.java
@@ -0,0 +1,9 @@
+package com.example.exa863_management_system_2023.dao.installation;
+
+import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.Installation;
+
+public interface InstallationDAO extends CRUD<Installation, Exception> {
+
+
+}
