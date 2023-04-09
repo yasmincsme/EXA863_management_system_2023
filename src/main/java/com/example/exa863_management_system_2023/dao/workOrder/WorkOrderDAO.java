de4262fbf740202364b6b93a38cb865314dde57b
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-09 00:29:14 -0300

    Update the interface "WorkOrderDAO"

---
+++ src/main/java/com/example/exa863_management_system_2023/dao/workOrder/WorkOrderDAO.java
@@ -0,0 +1,7 @@
+package com.example.exa863_management_system_2023.dao.workOrder;
+
+import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.WorkOrder;
+
+public interface WorkOrderDAO extends CRUD<WorkOrder, Exception> {
+}
