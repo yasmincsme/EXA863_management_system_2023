dc38103e74cb175bb600a30e92f5c346641b2a05
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-10 22:34:41 -0300

    Create the class "Manager"

---
+++ src/main/java/com/example/exa863_management_system_2023/model/Manager.java
@@ -0,0 +1,8 @@
+package com.example.exa863_management_system_2023.model;
+
+public class Manager extends User{
+
+    public Manager(String login, String password) {
+        super(login, password);
+    }
+}
