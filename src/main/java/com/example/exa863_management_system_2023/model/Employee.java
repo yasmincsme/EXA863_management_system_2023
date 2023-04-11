823b0f9b435968f8ecf8feb17a7aaf76817c8b2a
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-10 22:35:05 -0300

    Create the class "Employee"

---
+++ src/main/java/com/example/exa863_management_system_2023/model/Employee.java
@@ -0,0 +1,8 @@
+package com.example.exa863_management_system_2023.model;
+
+public class Employee extends User {
+
+    public Employee(String login, String password) {
+        super(login, password);
+    }
+}
