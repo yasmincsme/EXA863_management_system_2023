f574e64a131689b191daccdbbdb862ee9de61016
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-10 22:37:02 -0300

    Update the interface "CRUD"

--- src/main/java/com/example/exa863_management_system_2023/dao/CRUD.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/CRUD.java
@@ -6,13 +6,13 @@ public interface CRUD<T, E extends Exception> {
 
     public T create(T object);
 
-    public T findByID(int id);
+    public T findByID(String id);
 
     public List<T> findMany();
 
     public void update(T object) throws E;
 
-    public void delete(int id) throws E;
+    public void delete(String id) throws E;
 
     public void deleteMany();
 }
