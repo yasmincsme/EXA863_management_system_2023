682ecdc9a76b8f14806ffbc92719ef267b63cd55
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-15 19:21:17 -0300

    Update the class "Employee"

--- src/main/java/com/example/exa863_management_system_2023/model/Employee.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Employee.java
@@ -2,7 +2,7 @@ package com.example.exa863_management_system_2023.model;
 
 public class Employee extends User {
 
-    public Employee(String login, String password) {
-        super(login, password);
+    public Employee(String id, String login, String password) {
+        super(id, login, password);
     }
 }
