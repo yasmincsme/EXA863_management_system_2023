6ad6ed60f3382838a1c8903ed86b45519b8c049d
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-15 19:18:44 -0300

    Update the class "Client"

--- src/main/java/com/example/exa863_management_system_2023/model/Client.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Client.java
@@ -1,9 +1,12 @@
 package com.example.exa863_management_system_2023.model;
 
+import java.util.List;
+
 public class Client extends Person {
 
     private String address;
     private String phone;
+    private List<WorkOrder> workOrderList;
 
     public Client(String name) {
         super(name);
