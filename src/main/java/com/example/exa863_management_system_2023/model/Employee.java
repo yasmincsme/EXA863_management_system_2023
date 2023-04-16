a66e80a2be3bc774591bcce53f75c64b56dbb431
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-16 18:51:11 -0300

    Update the class "Employee"

--- src/main/java/com/example/exa863_management_system_2023/model/Employee.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Employee.java
@@ -2,7 +2,12 @@ package com.example.exa863_management_system_2023.model;
 
 public class Employee extends User {
 
-    public Employee(String id, String login, String password) {
-        super(id, login, password);
+    public Employee(String name, String email, String login, String password) {
+        super(name, email, login, password);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Role: Employee";
     }
 }
