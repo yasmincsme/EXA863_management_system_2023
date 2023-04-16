81a84c7973799838dceea7f236df47e1e44e347c
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-16 18:46:21 -0300

    Update the class "Manager"

--- src/main/java/com/example/exa863_management_system_2023/model/Manager.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Manager.java
@@ -2,7 +2,12 @@ package com.example.exa863_management_system_2023.model;
 
 public class Manager extends User{
 
-    public Manager(String id, String login, String password) {
-        super(id, login, password);
+    public Manager(String name, String email, String login, String password) {
+        super(name, email, login, password);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Role: Manager";
     }
 }
