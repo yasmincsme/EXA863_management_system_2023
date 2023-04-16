0665d38c8ae4587d7dd3e13c3c6c06233fd928fd
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-16 19:52:13 -0300

    Update the class "Employee" - Add an "equals" method

--- src/main/java/com/example/exa863_management_system_2023/model/Employee.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Employee.java
@@ -7,6 +7,17 @@ public class Employee extends User {
     }
 
     @Override
+    public boolean equals(Object object) {
+        if (object instanceof Employee) {
+            Employee employee = (Employee) object;
+            if (this.getID().equals(employee.getID())) {
+                return true;
+            }
+         }
+        return false;
+    }
+
+    @Override
     public String toString() {
         return super.toString() + ", Role: Employee";
     }
