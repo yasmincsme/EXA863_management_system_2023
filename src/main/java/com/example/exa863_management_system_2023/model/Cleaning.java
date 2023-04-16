2f90594ce66af11e29171b4df55d56179656f42a
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-16 18:45:41 -0300

    Update the class "Cleaning"

--- src/main/java/com/example/exa863_management_system_2023/model/Cleaning.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Cleaning.java
@@ -1,17 +1,20 @@
 package com.example.exa863_management_system_2023.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cleaning extends Service{
 
     private List<String> components;
 
-    public Cleaning(String name) {
-        super(name);
+    public Cleaning(long price, long cost) {
+        this.price = price;
+        this.cost = cost;
+        this.components = new ArrayList<>();
     }
 
     public List<String> getComponents() {
-        return getComponents();
+        return components;
     }
     public void setComponents(List<String> components) {
         this.components = components;
