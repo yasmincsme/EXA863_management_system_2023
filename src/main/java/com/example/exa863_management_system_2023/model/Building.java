a65af725dcce2cb2c9eee5731d1153c4ecef7c94
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-16 18:44:54 -0300

    Update the class "Building"

--- src/main/java/com/example/exa863_management_system_2023/model/Building.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Building.java
@@ -1,13 +1,14 @@
 package com.example.exa863_management_system_2023.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Building extends Service{
 
     private List<ComputerComponent> usedComponents;
 
-    public Building(String name) {
-        super(name);
+    public Building() {
+        this.usedComponents = new ArrayList<>();
     }
 
     public List<ComputerComponent> getUsedComponents() {
@@ -15,5 +16,27 @@ public class Building extends Service{
     }
     public void setUsedComponents(List<ComputerComponent> usedComponents) {
         this.usedComponents = usedComponents;
+    }
+
+    public void addComputerComponent(ComputerComponent component) {
+        this.usedComponents.add(component);
+    }
+
+    @Override
+    public long getPrice() {
+       long price = 0;
+       for (ComputerComponent component : this.usedComponents) {
+            price += component.getUnitPrice() * component.getQuantity();
+       }
+       return price;
+    }
+
+    @Override
+    public long getCost() {
+        long cost = 0;
+        for (ComputerComponent component : this.usedComponents) {
+            cost += component.getUnitCost() * component.getQuantity();
+        }
+        return cost;
     }
 }
