287d890a27a518aa84d7d10b409df52121d05a15
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-16 23:17:02 -0300

    Update the class "Building"

--- src/main/java/com/example/exa863_management_system_2023/model/Building.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Building.java
@@ -7,7 +7,8 @@ public class Building extends Service{
 
     private List<ComputerComponent> usedComponents;
 
-    public Building() {
+    public Building(String name, String description) {
+        super(name, description);
         this.usedComponents = new ArrayList<>();
     }
 
@@ -38,5 +39,21 @@ public class Building extends Service{
             cost += component.getUnitCost() * component.getQuantity();
         }
         return cost;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Building) {
+            Building building = (Building) object;
+            if(this.getID().equals(building.getID())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Service: Building" + ", Components: " + this.usedComponents;
     }
 }
