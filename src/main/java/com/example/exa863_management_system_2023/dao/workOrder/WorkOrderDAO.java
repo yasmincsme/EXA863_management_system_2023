08e8a14fd4e2df732199382897c3e99673273dbc
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-17 00:36:28 -0300

    Update the interface "WorkOrderDAO"

--- src/main/java/com/example/exa863_management_system_2023/dao/workOrder/WorkOrderDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/workOrder/WorkOrderDAO.java
@@ -3,5 +3,10 @@ package com.example.exa863_management_system_2023.dao.workOrder;
 import com.example.exa863_management_system_2023.dao.CRUD;
 import com.example.exa863_management_system_2023.model.WorkOrder;
 
+import java.util.List;
+
 public interface WorkOrderDAO extends CRUD<WorkOrder, Exception> {
+    List<WorkOrder> findIncompleteWorkOrders();
+    List<WorkOrder> findOrderByClientID(String id);
+    WorkOrder findOrderByTechnicianID(String id);
 }
