a8c09787fbf6459167e0008f857156d989997680
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-17 13:58:24 -0300

    Update the test "ComputerComponentTest" - Now it has high coverage

--- src/test/java/com/example/exa863_management_system_2023/model/ComputerComponentTest.java
+++ src/test/java/com/example/exa863_management_system_2023/model/ComputerComponentTest.java
@@ -8,31 +8,60 @@ public class ComputerComponentTest {
     @Test
     public void testConstructor() {
 
-        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "12345", 100, 200, 5);
+        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "ABC123", 100, 200, 5);
 
         assertEquals("CPU", component.getName());
         assertEquals("Central Processing Unit", component.getDescription());
         assertEquals("Intel", component.getManufacturer());
-        assertEquals("12345", component.getSerialNumber());
+        assertEquals("ABC123", component.getSerialNumber());
         assertEquals(100, component.getUnitCost());
         assertEquals(200, component.getUnitPrice());
         assertEquals(5, component.getQuantity());
     }
+
+    @Test
+    public void testGetCost() {
+        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "ABC123", 100, 200, 5);
+        assertEquals(500, component.getCost());
+    }
+
+    @Test
+    public void testGetPrice() {
+        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "ABC123", 100, 200, 5);
+        assertEquals(1000, component.getPrice());
+    }
+
     @Test
     public void testEquals() {
         ComputerComponent component1 = new ComputerComponent("CPU", "Intel Core i7", "Intel", "ABC123", 100, 150, 10);
+        component1.setID("11221ccc-dd3f-11ed");
         ComputerComponent component2 = new ComputerComponent("GPU", "Nvidia GeForce RTX 3080", "Nvidia", "XYZ789", 800, 1000, 5);
+        component2.setID("20012e04-dd3f-11ed");
         ComputerComponent component3 = new ComputerComponent("RAM", "Corsair Vengeance LPX", "Corsair", "DEF456", 50, 70, 20);
+        component3.setID("11221ccc-dd3f-11ed");
 
-        // Test equality of two different components with different IDs
+        //Test equality of two different components with different IDs
         assertNotEquals(component1, component2);
 
-        // Test equality of two different components with the same ID
+        //Test equality of two different components with the same ID
         assertNotEquals(component1, component3);
 
-        // Test equality of the same component
+        //Test equality of the same component
         assertEquals(component1, component1);
         assertEquals(component2, component2);
         assertEquals(component3, component3);
+
+        //Null object
+        assertNotEquals(null, component1);
     }
+
+    @Test
+    public void testToString() {
+        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "12345", 100, 200, 5);
+        component.setID("6e056406-dc97-11ed");
+        String expected = "ID: 6e056406-dc97-11ed, Description: Central Processing Unit, Manufacturer: Intel, Serial Number: ABC123, Unit Cost: 100, Unit Price: 200, Quantity: 5";
+        String actual = component.toString();
+        assertEquals(expected, actual);
+    }
+
 }
