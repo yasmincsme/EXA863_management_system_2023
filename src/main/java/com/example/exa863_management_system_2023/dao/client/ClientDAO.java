0793174b9c3ff05564d460cf6cb0107d4175cb9a
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-17 11:52:47 -0300

    Update the interface "ClientDAO"

--- src/main/java/com/example/exa863_management_system_2023/dao/client/ClientDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientDAO.java
@@ -6,6 +6,5 @@ import com.example.exa863_management_system_2023.model.Client;
 import java.util.List;
 
 public interface ClientDAO extends CRUD<Client, Exception> {
-
     public List<Client> findByName(String name);
 }
