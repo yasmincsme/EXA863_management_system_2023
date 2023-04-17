c97db669780b9f894791231fa4eb8b4db2001266
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-16 23:17:23 -0300

    Update the class "Cleaning"

--- src/main/java/com/example/exa863_management_system_2023/model/Cleaning.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Cleaning.java
@@ -5,18 +5,37 @@ import java.util.List;
 
 public class Cleaning extends Service{
 
-    private List<String> components;
+    private List<ComputerComponent> components;
 
-    public Cleaning(long price, long cost) {
-        this.price = price;
-        this.cost = cost;
+    public Cleaning(String name, String description, long price, long cost) {
+        super(name, description, price, cost);
         this.components = new ArrayList<>();
     }
 
-    public List<String> getComponents() {
+    public List<ComputerComponent> getComponents() {
         return components;
     }
-    public void setComponents(List<String> components) {
+    public void setComponents(List<ComputerComponent> components) {
         this.components = components;
+    }
+
+    public void addComponent(ComputerComponent component) {
+        this.components.add(component);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Cleaning) {
+            Cleaning cleaning = (Cleaning) object;
+            if (this.getID().equals(cleaning.getID())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Service: Cleaning" + ", Components: " + this.components;
     }
 }
