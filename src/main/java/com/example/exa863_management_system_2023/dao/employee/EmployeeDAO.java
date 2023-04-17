6f36d939744dafa0bcccff293e67f7a0f91cfaa4
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-17 11:53:02 -0300

    Update the interface "EmployeeDAO"

--- src/main/java/com/example/exa863_management_system_2023/dao/employee/EmployeeDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/employee/EmployeeDAO.java
@@ -6,6 +6,5 @@ import com.example.exa863_management_system_2023.model.Employee;
 import java.util.List;
 
 public interface EmployeeDAO extends CRUD<Employee, Exception> {
-
     public List<Employee> findByLogin(String login);
 }
