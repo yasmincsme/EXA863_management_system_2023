210d25a1b14e35e4639eddee1c2f249a7b2ef5e8
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-17 11:53:36 -0300

    Update the interface "ManagerDAO"

--- src/main/java/com/example/exa863_management_system_2023/dao/manager/ManagerDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/manager/ManagerDAO.java
@@ -7,6 +7,5 @@ import com.example.exa863_management_system_2023.model.Manager;
 import java.util.List;
 
 public interface ManagerDAO extends CRUD<Manager, Exception> {
-
     public List<Manager> findByLogin(String login);
 }
