9f7bc562af26947a6cab3f9cab208db682316b9c
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-20 00:37:33 -0300

    :test_tube: test: Atualização do teste

--- src/test/java/com/example/exa863_management_system_2023/model/ComputerComponentTest.java
+++ src/test/java/com/example/exa863_management_system_2023/model/ComputerComponentTest.java
@@ -8,43 +8,33 @@ public class ComputerComponentTest {
     @Test
     public void testFirstConstructor() {
 
-        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "ABC123", 100, 200, 5);
+        ComputerComponent component = new ComputerComponent("CPU", "Intel", "ABC123", 200.00, 100.00, 5);
 
         assertEquals("CPU", component.getName());
-        assertEquals("Central Processing Unit", component.getDescription());
         assertEquals("Intel", component.getManufacturer());
         assertEquals("ABC123", component.getSerialNumber());
-        assertEquals(100, component.getUnitCost());
-        assertEquals(200, component.getUnitPrice());
+        assertEquals(200.00, component.getUnitPrice());
+        assertEquals(100.00, component.getUnitCost());
         assertEquals(5, component.getQuantity());
     }
 
     @Test
-    public void testSecondConstructor() {
-        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel");
-
-        assertEquals("CPU", component.getName());
-        assertEquals("Central Processing Unit", component.getDescription());
-        assertEquals("Intel", component.getManufacturer());
-    }
-
-    @Test
     public void testGetCost() {
-        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "ABC123", 100, 200, 5);
+        ComputerComponent component = new ComputerComponent("CPU", "Intel", "ABC123", 100, 200, 5);
         assertEquals(500, component.getCost());
     }
 
     @Test
     public void testGetPrice() {
-        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "ABC123", 100, 200, 5);
+        ComputerComponent component = new ComputerComponent("CPU", "Intel", "ABC123", 100, 200, 5);
         assertEquals(1000, component.getPrice());
     }
 
     @Test
     public void testEquals() {
-        ComputerComponent component1 = new ComputerComponent("CPU", "Intel Core i7", "Intel", "ABC123", 100, 150, 10);
+        ComputerComponent component1 = new ComputerComponent("CPU", "Intel", "ABC123", 100, 150, 10);
         component1.setID("11221ccc-dd3f-11ed");
-        ComputerComponent component2 = new ComputerComponent("GPU", "Nvidia GeForce RTX 3080", "Nvidia", "XYZ789", 800, 1000, 5);
+        ComputerComponent component2 = new ComputerComponent("GPU", "Nvidia", "XYZ789", 800, 1000, 5);
         component2.setID("20012e04-dd3f-11ed");
 
         //Test equality of two different components with different IDs
@@ -60,9 +50,9 @@ public class ComputerComponentTest {
 
     @Test
     public void testToString() {
-        ComputerComponent component = new ComputerComponent("CPU", "Central Processing Unit", "Intel", "ABC123", 100, 200, 5);
+        ComputerComponent component = new ComputerComponent("CPU", "Intel", "ABC123", 100, 200, 5);
         component.setID("6e056406-dc97-11ed");
-        String expected = "ID: 6e056406-dc97-11ed, Name: CPU, Description: Central Processing Unit, Manufacturer: Intel, Serial Number: ABC123, Unit Cost: 100, Unit Price: 200, Quantity: 5";
+        String expected = "ID: 6e056406-dc97-11ed, Name: CPU, Manufacturer: Intel, Serial Number: ABC123, Unit Cost: 100, Unit Price: 200, Quantity: 5";
         String actual = component.toString();
         assertEquals(expected, actual);
     }
