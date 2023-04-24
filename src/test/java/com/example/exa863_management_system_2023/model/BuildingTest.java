49a4ff3105c95e2b4c7eed9219e9d85dbaaa026f
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-23 23:48:31 -0300

    :test_tube: test: Correção de grafia dentro do teste

--- src/test/java/com/example/exa863_management_system_2023/model/BuildingTest.java
+++ src/test/java/com/example/exa863_management_system_2023/model/BuildingTest.java
@@ -12,59 +12,74 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BuildingTest {
 
-    private final List<ComputerComponent> stock = new ArrayList<>();
-
-    @BeforeEach
-    public void setUp() {
-        ComputerComponent component1 = new ComputerComponent("CPU", "Intel", "ABC123", 100, 150, 10);
-        component1.setID("11221ccc-dd3f-11ed");
-        ComputerComponent component2 = new ComputerComponent("GPU", "Nvidia", "XYZ789", 800, 1000, 5);
-        component2.setID("20012e04-dd3f-11ed");
-        ComputerComponent component3 = new ComputerComponent("RAM", "Corsair", "DEF456", 50, 70, 20);
-        component3.setID("11221ccc-dd3f-11ed");
-        stock.add(component1);
-        stock.add(component2);
-        stock.add(component3);
-
-        assertEquals(3, stock.size());
-    }
-
     @Test
     public void testConstructor() {
 
-        Building building = new Building("Change de Hard Disk", "Safely remove the old hard disk and install a new one", 120, 60);
+        Building building = new Building("Switch the HD", 70.00, 25.00);
 
-        assertEquals("Change de Hard Disk", building.getName());
-        assertEquals("Safely remove the old hard disk and install a new one", building.getDescription());
-        assertEquals(120, building.getPrice());
-        assertEquals(60, building.getCost());
-        assertNotNull(building.getUsedComponents());
+        assertNull(building.getID());
+        assertEquals("Switch the HD", building.getDescription());
+        assertEquals(70.00, building.getPrice(), 3);
+        assertEquals(25.00, building.getCost(), 3);
         assertTrue(building.getUsedComponents().isEmpty());
     }
 
     @Test
+    public void testIncrease() {
+        Building building = new Building("Switch the HD", 70.00, 25.00);
+        building.setID("c28c7bbe-dd45-11ed");
+
+        assertTrue(building.getUsedComponents().isEmpty());
+
+        ComputerComponent component1 = new ComputerComponent("CPU", "Intel", "ABC123", 200.00, 100.00, 2);
+        component1.setID("a908ffa4-dd37-11ed");
+        ComputerComponent component2 = new ComputerComponent("RAM", "Intel", "XYZ456", 300.00, 200.00, 2);
+        component2.setID("ed198b64-dd37-11ed");
+
+        ArrayList<ComputerComponent> usedComponents = new ArrayList<>();
+        usedComponents.add(component1);
+        usedComponents.add(component2);
+        building.setUsedComponents(usedComponents);
+
+        assertFalse(building.getUsedComponents().isEmpty());
+
+        building.increasePrice();
+        assertEquals(1070.00, building.getPrice(), 3);
+
+        building.increaseCost();
+        assertEquals(625.00, building.getCost(), 3);
+
+    }
+
+    @Test
     public void testEquals() {
-        Building building1 = new Building("Change de Hard Disk", "Safely remove the old hard disk and install a new one", 120, 60);
-        building1.setID("68e7e64c-dd4b-11ed");
-        Building building2 = new Building("RAM Upgrade", "Install additional RAM modules to increase memory capacity", 60, 30);
-        building2.setID("72980f1e-dd4b-11ed");
+        Building building1 = new Building("Switch the HD", 70.00, 25.00);
+        building1.setID("c28c7bbe-dd45-11ed");
+        Building building2 = new Building("Switch the RAM", 90.00, 65.00);
+        building2.setID("d9c0adb4-dd45-11ed");
+        Building building3 = new Building("Switch the CPU", 300.00, 120.00);
+        building3.setID("c28c7bbe-dd45-11ed");
 
-        //Test equality of two different components with different IDs
+        //Verify that two clients with the same ID are equal
+        assertEquals(building1, building3);
+
+        //Verify that two clients with different IDs are not equal
         assertNotEquals(building1, building2);
-
-        //Test equality of the same component
-        assertEquals(building1, building1);
-        assertEquals(building2, building2);
 
         //Null object
         assertNotEquals(null, building1);
+
+        //Same object
+        assertEquals(building1, building1);
+        assertEquals(building2, building2);
+        assertEquals(building3, building3);
     }
 
     @Test
     public void testToString() {
-        Building building = new Building("Change de Hard Disk", "Safely remove the old hard disk and install a new one", 120, 60);
-        building.setID("68e7e64c-dd4b-11ed");
-        String expected = "ID: 68e7e64c-dd4b-11ed, Name: Change de Hard Disk, Description: Safely remove the old hard disk and install a new one, Price: 120, Cost: 60, Service: Building, Components: []";
+        Building building = new Building("Switch the HD", 70.00, 25.00);
+        building.setID("c28c7bbe-dd45-11ed");
+        String expected = "Type: BUILDING, ID: c28c7bbe-dd45-11ed, Description: Switch the HD, Price: 70.0, Cost: 25.0, Used Components: []";
         String actual = building.toString();
         assertEquals(expected, actual);
     }
