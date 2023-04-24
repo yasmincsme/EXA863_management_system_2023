aadbd9d6c150bd71e52ebd9cbcaa5c9bca52f929
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-24 14:17:04 -0300

    :test_tube: test: Adaptando o teste à nova funcionalidade da classe

--- src/test/java/com/example/exa863_management_system_2023/model/CleaningTest.java
+++ src/test/java/com/example/exa863_management_system_2023/model/CleaningTest.java
@@ -12,9 +12,10 @@ public class CleaningTest {
     @Test
     public void testConstructor() {
 
-        Cleaning cleaning = new Cleaning("CPU cooler cleaning", 70.00, 25.00);
+        Cleaning cleaning = new Cleaning("CPU cooler cleaning", "c28c7bbe-dd45-11ed", 70.00, 25.00);
 
         assertNull(cleaning.getID());
+        assertEquals("c28c7bbe-dd45-11ed", cleaning.getWorkOrderID());
         assertEquals("CPU cooler cleaning", cleaning.getDescription());
         assertEquals(70, cleaning.getPrice(), 3);
         assertEquals(25, cleaning.getCost(), 3);
@@ -22,11 +23,11 @@ public class CleaningTest {
 
     @Test
     public void testEquals() {
-        Cleaning cleaning1 = new Cleaning("CPU cooler cleaning", 70.00, 25.00);
+        Cleaning cleaning1 = new Cleaning("CPU cooler cleaning", "f28c7bbe-dd45-11ed", 70.00, 25.00);
         cleaning1.setID("c28c7bbe-dd45-11ed");
-        Cleaning cleaning2 = new Cleaning("Clean keyboard", 35.00, 5.00);
+        Cleaning cleaning2 = new Cleaning("Clean keyboard", "f28c7bbe-dd45-11ed", 35.00, 5.00);
         cleaning2.setID("d9c0adb4-dd45-11ed");
-        Cleaning cleaning3 = new Cleaning("Cleaning peripheral components", 60.00, 10.00);
+        Cleaning cleaning3 = new Cleaning("Cleaning peripheral components", "f28c7bbe-dd45-11ed", 60.00, 10.00);
         cleaning3.setID("c28c7bbe-dd45-11ed");
 
         //Verify that two clients with the same ID are equal
@@ -46,9 +47,9 @@ public class CleaningTest {
 
     @Test
     public void testToString() {
-        Cleaning cleaning = new Cleaning("Cleaning peripheral components", 70.00, 25.00);
+        Cleaning cleaning = new Cleaning("Cleaning peripheral components", "f28c7bbe-dd45-11ed", 70.00, 25.00);
         cleaning.setID("c28c7bbe-dd45-11ed");
-        String expected = "Type: CLEANING, ID: c28c7bbe-dd45-11ed, Description: Cleaning peripheral components, Price: 70.0, Cost: 25.0";
+        String expected = "Type: CLEANING, ID: c28c7bbe-dd45-11ed, Order's ID: f28c7bbe-dd45-11ed, Description: Cleaning peripheral components, Price: 70.0, Cost: 25.0";
         String actual = cleaning.toString();
         assertEquals(expected, actual);
     }
