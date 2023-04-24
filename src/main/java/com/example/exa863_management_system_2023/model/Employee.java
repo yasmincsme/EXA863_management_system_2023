290dd1b66d7c878ea1691533a2d9b6044e2e35fa
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-24 00:14:52 -0300

    :boom: fix: Corrigindo erros acusados nos testes

--- src/main/java/com/example/exa863_management_system_2023/model/Employee.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Employee.java
@@ -107,13 +107,13 @@ public class Employee {
     /**
      *
      * @param object Receive a random object
-     * @return Return true if the given object is equal to the current Manager object, or false if they are not the same
+     * @return Return true if the given object is equal to the current Employee object, or false if they are not the same
      */
     @Override
     public boolean equals(Object object) {
-        if(object instanceof ComputerComponent) {
-            ComputerComponent component = (ComputerComponent) object;
-            if (this.getID().equals(component.getID())) {
+        if(object instanceof Employee) {
+            Employee employee = (Employee) object;
+            if (this.getID().equals(employee.getID())) {
                 return true;
             }
         }
