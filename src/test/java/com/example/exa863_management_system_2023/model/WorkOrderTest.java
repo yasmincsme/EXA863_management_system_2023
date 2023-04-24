c486f00302ca516d1d0f797f652e350a5dddb52e
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-24 14:16:06 -0300

    :test_tube: test: Criando novo teste

--- src/test/java/com/example/exa863_management_system_2023/model/WorkOrderTest.java
+++ src/test/java/com/example/exa863_management_system_2023/model/WorkOrderTest.java
@@ -1,137 +1,30 @@
 package com.example.exa863_management_system_2023.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WorkOrderTest {
 
-    private WorkOrder workOrder;
-    private String clientID;
-    private String description;
-    private Building building;
-    private Cleaning cleaning;
-    private Installation installation;
+    @Test
+    public void testConstructor() {
 
-    @BeforeEach
-    void setUp() {
-        clientID = "c28c7bbe-dd45-11ed";
-        description = "Install additional RAM modules to increase memory capacity";
-        workOrder = new WorkOrder(clientID, description);
-        building = new Building("Change RAM", "Safely remove the RAM memory and install a new one", 120, 60);
-        cleaning = new Cleaning("Cleaning peripheral components", "Remove dirt and grime", 70, 25);
-        installation = new Installation("Installation of the Office package and antivirus", "Provide a seamless and hassle-free installation of the Microsoft Office package and a reliable antivirus software", 100, 0, "Windows");
+        WorkOrder workOrder = new WorkOrder("c28c7bbe-dd45-11ed", "Computer hardware upgrade");
+        workOrder.setCreatedAt("24-04-2023 14:05:35.377");
+
+        assertNull(workOrder.getID());
+        assertEquals("c28c7bbe-dd45-11ed", workOrder.getClientID());
+        assertNull(workOrder.getTechnicianID());
+        assertEquals("OPEN", workOrder.getStatus());
+        assertEquals("Computer hardware upgrade", workOrder.getDescription());
+        assertEquals("24-04-2023 14:05:35.377", workOrder.getCreatedAt());
+        assertNull(workOrder.getFinishedAt());
+        assertEquals(0, workOrder.getClientSatisfaction());
+        assertEquals(0.00, workOrder.getPrice());
+        assertEquals(0.00, workOrder.getCost());
+        assertNull(workOrder.getPaymentMethod());
     }
 
     @Test
-    void testAddBuildingService() {
-        workOrder.addBuildingService(building);
-        List<Building> expectedBuildingList = new ArrayList<>();
-        expectedBuildingList.add(building);
-        assertEquals(expectedBuildingList, workOrder.getBuildindList());
-    }
 
-    @Test
-    void testAddCleaningService() {
-        workOrder.addCleaningService(cleaning);
-        List<Cleaning> expectedCleaningList = new ArrayList<>();
-        expectedCleaningList.add(cleaning);
-        assertEquals(expectedCleaningList, workOrder.getCleaningList());
-    }
-
-    @Test
-    void testAddInstallationService() {
-        workOrder.addInstallationService(installation);
-        List<Installation> expectedInstallationList = new ArrayList<>();
-        expectedInstallationList.add(installation);
-        assertEquals(expectedInstallationList, workOrder.getInstallationList());
-    }
-
-    @Test
-    void testGetBuildingListPrice() {
-        workOrder.addBuildingService(building);
-        assertEquals(120, workOrder.getBuildingListPrice());
-    }
-
-    @Test
-    void testGetCleaningListPrice() {
-        workOrder.addCleaningService(cleaning);
-        assertEquals(70, workOrder.getCleaningListPrice());
-    }
-
-    @Test
-    void testGetInstallationListPrice() {
-        workOrder.addInstallationService(installation);
-        assertEquals(100, workOrder.getInstallationListPrice());
-    }
-
-    @Test
-    void testGetPrice() {
-        workOrder.addBuildingService(building);
-        workOrder.addCleaningService(cleaning);
-        workOrder.addInstallationService(installation);
-        assertEquals(290, workOrder.getPrice());
-    }
-
-    @Test
-    void testGetBuildingListCost() {
-        workOrder.addBuildingService(building);
-        assertEquals(60, workOrder.getBuildingListCost());
-    }
-
-    @Test
-    void testGetCleaningListCost() {
-        workOrder.addCleaningService(cleaning);
-        assertEquals(25, workOrder.getCleaningListCost());
-    }
-
-    @Test
-    void testGetInstallationListCost() {
-        workOrder.addInstallationService(installation);
-        assertEquals(0, workOrder.getInstallationListCost());
-    }
-
-    @Test
-    void testGetCost() {
-        workOrder.addBuildingService(building);
-        workOrder.addCleaningService(cleaning);
-        workOrder.addInstallationService(installation);
-        assertEquals(85, workOrder.getCost());
-    }
-
-    @Test
-    void testStatus() {
-        assertTrue(workOrder.isOnGoing());
-        assertFalse(workOrder.isFinished());
-        assertFalse(workOrder.isCanceled());
-
-        workOrder.finish();
-        assertFalse(workOrder.isOnGoing());
-        assertTrue(workOrder.isFinished());
-        assertFalse(workOrder.isCanceled());
-
-        workOrder.cancel();
-        assertFalse(workOrder.isOnGoing());
-        assertFalse(workOrder.isFinished());
-        assertTrue(workOrder.isCanceled());
-    }
-
-    @Test
-    void waitingTime() {
-        WorkOrder workOrder = new WorkOrder("c28c7bbe-dd45-11ed", "Install additional RAM modules to increase memory capacity");
-
-        workOrder.setCreatedAt(Date.from(LocalDate.of(2023, 4, 15).atStartOfDay(ZoneId.systemDefault()).toInstant()));
-        workOrder.setFinishedAt(Date.from(LocalDate.of(2023, 4, 17).atStartOfDay(ZoneId.systemDefault()).toInstant()));
-
-        assertEquals(2, workOrder.getWaitingTime());
-
-    }
 }
