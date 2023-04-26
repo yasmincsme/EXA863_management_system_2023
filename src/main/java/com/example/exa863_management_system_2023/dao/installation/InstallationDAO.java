929e1c7b9e1c39af635f439d156d0ebe816e1461
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-25 23:11:46 -0300

    :sparkles: feat: Adicionando assinatura do método "findByWorkOrderID"

--- src/main/java/com/example/exa863_management_system_2023/dao/installation/InstallationDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/installation/InstallationDAO.java
@@ -1,7 +1,11 @@
 package com.example.exa863_management_system_2023.dao.installation;
 
 import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.Building;
 import com.example.exa863_management_system_2023.model.Installation;
 
+import java.util.List;
+
 public interface InstallationDAO extends CRUD<Installation, Exception> {
+    public List<Installation> findByWorkOrderID(String workOrderID);
 }
