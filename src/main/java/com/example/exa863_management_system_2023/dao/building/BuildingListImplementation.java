c98e28605dbbcade18e87e912d4a11093dfdd7ff
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-25 23:10:57 -0300

    :sparkles: feat: Adicionando método "findByWorkOrderID"

--- src/main/java/com/example/exa863_management_system_2023/dao/building/BuildingListImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/building/BuildingListImplementation.java
@@ -1,36 +1,33 @@
 package com.example.exa863_management_system_2023.dao.building;
 
 import com.example.exa863_management_system_2023.model.Building;
+import com.example.exa863_management_system_2023.utils.Generator;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
 
 public class BuildingListImplementation implements BuildingDAO {
 
-    private List<Building> listOfBuilding;
+    private List<Building> listOfBuildings;
     private String nextID;
 
     public BuildingListImplementation() {
-        this.listOfBuilding = new ArrayList<Building>();
-        UUID uuid = UUID.randomUUID();
-        this.nextID = uuid.toString();
+        this.listOfBuildings = new ArrayList<Building>();
+        this.nextID = Generator.generateID();
     }
 
     @Override
     public Building create(Building building) {
         building.setID(this.nextID);
-        UUID uuid = UUID.randomUUID();
-        this.nextID = uuid.toString();
-        this.listOfBuilding.add(building);
+        this.nextID = Generator.generateID();
+        this.listOfBuildings.add(building);
         return building;
     }
 
     @Override
     public List<Building> findMany() {
         List<Building> buildingList = new ArrayList<Building>();
-        for (Building building : this.listOfBuilding) {
+        for (Building building : this.listOfBuildings) {
             buildingList.add(building);
         }
         return buildingList;
@@ -38,7 +35,7 @@ public class BuildingListImplementation implements BuildingDAO {
 
     @Override
     public Building findByID(String id) {
-        for (Building building : this.listOfBuilding) {
+        for (Building building : this.listOfBuildings) {
             if (building.getID().equals(id)) {
                 return building;
             }
@@ -47,19 +44,31 @@ public class BuildingListImplementation implements BuildingDAO {
     }
 
     @Override
+    public List<Building> findByWorkOrderID(String workOrderID) {
+        List<Building> orderBuildingList = new ArrayList<>();
+        for (Building building : this.listOfBuildings) {
+            if (building.getWorkOrderID().equals(workOrderID)) {
+                orderBuildingList.add(building);
+            }
+        }
+        return orderBuildingList;
+    }
+
+    @Override
     public void update(Building building) throws Exception {
-        for (int i = 0; i < this.listOfBuilding.size(); i++) {
-            if (this.listOfBuilding.get(i).getID().equals(building.getID())) {
-                this.listOfBuilding.set(i, building);
+        for (int i = 0; i < this.listOfBuildings.size(); i++) {
+            if (this.listOfBuildings.get(i).getID().equals(building.getID())) {
+                this.listOfBuildings.set(i, building);
+                return;
             }
         }
     }
 
     @Override
     public void delete(String id) throws Exception {
-        for (int i = 0; i < this.listOfBuilding.size(); i++) {
-            if (this.listOfBuilding.get(i).getID().equals(id)) {
-                this.listOfBuilding.remove(i);
+        for (int i = 0; i < this.listOfBuildings.size(); i++) {
+            if (this.listOfBuildings.get(i).getID().equals(id)) {
+                this.listOfBuildings.remove(i);
                 return;
             }
         }
@@ -67,7 +76,7 @@ public class BuildingListImplementation implements BuildingDAO {
 
     @Override
     public void deleteMany() {
-        this.listOfBuilding = new ArrayList<>();
+        this.listOfBuildings = new ArrayList<>();
         this.nextID = null;
     }
 }
