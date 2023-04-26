2e1e823db299d2b24bf7493bf28d1d0e18206ec7
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-25 22:17:36 -0300

    :sparkles: feat: Adicionando novos atributos

--- src/main/java/com/example/exa863_management_system_2023/dao/DAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/DAO.java
@@ -1,10 +1,27 @@
 package com.example.exa863_management_system_2023.dao;
 
+import com.example.exa863_management_system_2023.dao.building.BuildingDAO;
+import com.example.exa863_management_system_2023.dao.cleaning.CleaningDAO;
 import com.example.exa863_management_system_2023.dao.client.ClientDAO;
+import com.example.exa863_management_system_2023.dao.computerComponent.ComputerComponentDAO;
+import com.example.exa863_management_system_2023.dao.employee.EmployeeDAO;
+import com.example.exa863_management_system_2023.dao.installation.InstallationDAO;
+import com.example.exa863_management_system_2023.dao.manager.ManagerDAO;
+import com.example.exa863_management_system_2023.dao.technician.TechnicianDAO;
+import com.example.exa863_management_system_2023.dao.workOrder.WorkOrderDAO;
 
 public class DAO {
 
-    private static ClientDAO clientDAO;
+    private static BuildingDAO buildingDAO;
+    private static CleaningDAO cleaningDAO;
+    private static InstallationDAO installationDAO;
 
+    private static ManagerDAO managerDAO;
+    private static EmployeeDAO employeeDAO;
+    private static ClientDAO clientDAO;
+    private static TechnicianDAO technicianDAO;
+
+    private static ComputerComponentDAO computerComponentDAO;
+    private static WorkOrderDAO workOrderDAO;
 
 }
