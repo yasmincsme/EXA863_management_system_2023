dbfda241092c9144cd35def8cc52793b284108c9
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-25 23:12:00 -0300

    :sparkles: feat: Adicionando assinatura do método "findByWorkOrderID"

--- src/main/java/com/example/exa863_management_system_2023/dao/cleaning/CleaningDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/cleaning/CleaningDAO.java
@@ -1,7 +1,11 @@
 package com.example.exa863_management_system_2023.dao.cleaning;
 
 import com.example.exa863_management_system_2023.dao.CRUD;
+import com.example.exa863_management_system_2023.model.Building;
 import com.example.exa863_management_system_2023.model.Cleaning;
 
+import java.util.List;
+
 public interface CleaningDAO extends CRUD<Cleaning, Exception> {
+    public List<Cleaning> findByWorkOrderID(String workOrderID);
 }
