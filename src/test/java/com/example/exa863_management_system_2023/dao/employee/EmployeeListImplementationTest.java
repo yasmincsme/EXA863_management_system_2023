1f3a3caff9814b5f5076cb832174f194ec4f5d47
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-29 17:29:05 -0300

    :recycle: refactor: Remodelando o teste

--- src/test/java/com/example/exa863_management_system_2023/dao/employee/EmployeeListImplementationTest.java
+++ src/test/java/com/example/exa863_management_system_2023/dao/employee/EmployeeListImplementationTest.java
@@ -2,44 +2,34 @@ package com.example.exa863_management_system_2023.dao.employee;
 
 import com.example.exa863_management_system_2023.dao.DAO;
 import com.example.exa863_management_system_2023.model.Employee;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-
-import java.util.ArrayList;
+import org.junit.jupiter.api.*;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeListImplementationTest {
-    private static Employee employee1;
-    private static Employee employee2;
+    private Employee employee1;
+    private Employee employee2;
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         employee1 = DAO.getEmployee().create(new Employee("Lionel Craig", "lionel.craig@example.com", "lionelCraig", "password123"));
         employee2 = DAO.getEmployee().create(new Employee("Jane Doe", "jane.doe@example.com", "janeDoe", "password123"));
     }
 
-    /*@AfterAll
-    public static void tearDown() {
+    @AfterEach
+    public void tearDown() {
         DAO.getEmployee().deleteMany();
-    }*/
+    }
+
+    @Test
+    public void testCreate() {
+        Employee actual = DAO.getEmployee().create(new Employee("Lionel Craig", "lionel.craig@example.com", "lionelCraig", "password123"));
+        Employee expected = DAO.getEmployee().findByID(actual.getID());
+        assertEquals(expected, actual);
+    }
 
     @Test
     public void testFindMany() {
-
-        employee1 = DAO.getEmployee().create(new Employee("Lionel Craig", "lionel.craig@example.com", "lionelCraig", "password123"));
-        employee2 = DAO.getEmployee().create(new Employee("Jane Doe", "jane.doe@example.com", "janeDoe", "password123"));
-
-        System.out.println(DAO.getEmployee().findMany());
-        System.out.println(employee1);
-        System.out.println(DAO.getEmployee().findByLogin("janeDoe"));
-        System.out.println(DAO.getEmployee().findByLogin("lionelCraig"));
-
-        System.out.println(employee2);
         List<Employee> employeeList = DAO.getEmployee().findMany();
         int expectedLength = 2;
         assertEquals(expectedLength, employeeList.size());
@@ -47,10 +37,7 @@ public class EmployeeListImplementationTest {
 
     @Test
     public void testFindByID() {
-        Employee expected = DAO.getEmployee().create(new Employee("Lionel Craig", "lionel.craig@example.com", "lionelCraig", "password123"));
-        String expectedID = expected.getID();
-        Employee actual = DAO.getEmployee().findByID(expectedID);
-
-        assertEquals(expected, actual);
+        Employee actual = DAO.getEmployee().findByID(employee1.getID());
+        assertEquals(employee1, actual);
     }
 }
