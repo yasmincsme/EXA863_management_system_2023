97a2d34e72d9722e60db17ce3b3b49fbdd8b9ef5
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-29 21:28:30 -0300

    :recycle: refactor: Realizando correções de erros apontados nos testes

--- src/main/java/com/example/exa863_management_system_2023/dao/CRUD.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/CRUD.java
@@ -1,5 +1,7 @@
 package com.example.exa863_management_system_2023.dao;
 
+import com.example.exa863_management_system_2023.Exceptions.ObjectNotFoundException;
+
 import java.util.List;
 
 public interface CRUD<T, E extends Exception> {
@@ -10,9 +12,9 @@ public interface CRUD<T, E extends Exception> {
 
     public List<T> findMany();
 
-    public void update(T object) throws E;
+    public void update(T object) throws ObjectNotFoundException;
 
-    public void delete(String id) throws E;
+    public void delete(String id) throws ObjectNotFoundException;
 
     public void deleteMany();
 }
