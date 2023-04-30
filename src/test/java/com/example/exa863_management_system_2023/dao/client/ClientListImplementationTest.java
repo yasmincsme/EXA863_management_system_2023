6ad6c698ad3520626e5c6ca3ee5eec85b753468b
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-29 21:22:04 -0300

    :test_tube: test: Modelando teste para a classe

---
+++ src/test/java/com/example/exa863_management_system_2023/dao/client/ClientListImplementationTest.java
@@ -0,0 +1,90 @@
+package com.example.exa863_management_system_2023.dao.client;
+
+import com.example.exa863_management_system_2023.Exceptions.ObjectNotFoundException;
+import com.example.exa863_management_system_2023.dao.DAO;
+import com.example.exa863_management_system_2023.model.Client;
+import org.junit.jupiter.api.*;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClientListImplementationTest {
+    private Client client1;
+    private Client client2;
+
+    @BeforeEach
+    public void setUp() {
+        client1 = DAO.getClient().create(new Client("John Smith", "john.smith@example.com", "245 Cold Storage Rd, Craig, Alaska 99921, USA", "password123"));
+        client2 = DAO.getClient().create(new Client("Jane Dame", "jane.dame@example.com", "257 Fireweed Ln, Ketchikan, Alaska 99901, USA", "password123"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        DAO.getClient().deleteMany();
+    }
+
+    @Test
+    public void testCreate() {
+        Client actual = DAO.getClient().create(new Client("John Smith", "john.smith@example.com", "245 Cold Storage Rd, Craig, Alaska 99921, USA", "password123"));
+        Client expected = DAO.getClient().findByID(actual.getID());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindMany() {
+        List<Client> clientList = DAO.getClient().findMany();
+        int expectedLength = 2;
+        assertEquals(expectedLength, clientList.size());
+    }
+
+    @Test
+    public void testFindByID() {
+        Client actual1 = DAO.getClient().findByID(client1.getID());
+        assertEquals(client1, actual1);
+
+        Client actual2 = DAO.getClient().findByID((client2.getID()));
+        assertEquals(client2, actual2);
+    }
+
+    @Test
+    public void testFindByName() {
+        List<Client> actual1 = DAO.getClient().findByName(client1.getName());
+        assertEquals(client1, actual1.get(0));
+
+        List<Client> actual2 = DAO.getClient().findByName(client2.getName());
+        assertEquals(client2, actual2.get(0));
+    }
+
+    @Test
+    public void testUpdate() {
+        String client1ID = client1.getID();
+        Client newClient1 = DAO.getClient().create(new Client("John Smith", "john.smith@example.com", "245 Cold Storage Rd, Craig, Alaska 99921, USA", "password123"));
+        newClient1.setID(client1ID);
+
+        try {
+            DAO.getClient().update(newClient1);
+        } catch (ObjectNotFoundException exception) {
+            throw new RuntimeException(exception);
+        }
+        assertEquals(newClient1, DAO.getClient().findByID(client1ID));
+    }
+
+    @Test
+    public void testDelete() {
+        try {
+            DAO.getClient().delete(client1.getID());
+        } catch (ObjectNotFoundException exception) {
+            throw new RuntimeException(exception);
+        }
+        List<Client> clientList = DAO.getClient().findMany();
+        int expectedLength = 1;
+        assertEquals(1, clientList.size());
+        assertEquals(client2, clientList.get(0));
+    }
+
+    @Test
+    public void testDeleteMany() {
+        DAO.getClient().deleteMany();
+        List<Client> clientList = DAO.getClient().findMany();
+        assertEquals(0, clientList.size());
+    }
+}
--- src/test/java/com/example/exa863_management_system_2023/dao/employee/EmployeeListImplementationTest.java
+++ src/test/java/com/example/exa863_management_system_2023/dao/employee/EmployeeListImplementationTest.java
@@ -1,8 +1,10 @@
 package com.example.exa863_management_system_2023.dao.employee;
 
+import com.example.exa863_management_system_2023.Exceptions.ObjectNotFoundException;
 import com.example.exa863_management_system_2023.dao.DAO;
 import com.example.exa863_management_system_2023.model.Employee;
 import org.junit.jupiter.api.*;
+
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,7 +39,53 @@ public class EmployeeListImplementationTest {
 
     @Test
     public void testFindByID() {
-        Employee actual = DAO.getEmployee().findByID(employee1.getID());
-        assertEquals(employee1, actual);
+        Employee actual1 = DAO.getEmployee().findByID(employee1.getID());
+        assertEquals(employee1, actual1);
+
+        Employee actual2 = DAO.getEmployee().findByID((employee2.getID()));
+        assertEquals(employee2, actual2);
+    }
+
+    @Test
+    public void testFindByLogin() {
+        List<Employee> actual1 = DAO.getEmployee().findByLogin(employee1.getLogin());
+        assertEquals(employee1, actual1.get(0));
+
+        List<Employee> actual2 = DAO.getEmployee().findByLogin(employee2.getLogin());
+        assertEquals(employee2, actual2.get(0));
+    }
+
+    @Test
+    public void testUpdate() {
+        String employee1ID = employee1.getID();
+        Employee newEmployee1 = DAO.getEmployee().create(new Employee("Lionel Craig", "lionel.craig@example.com", "lionelCraig", "newPassword123"));
+        newEmployee1.setID(employee1ID);
+
+        try {
+            DAO.getEmployee().update(newEmployee1);
+        } catch (ObjectNotFoundException exception) {
+            throw new RuntimeException(exception);
+        }
+        assertEquals(newEmployee1, DAO.getEmployee().findByID(employee1ID));
+    }
+
+    @Test
+    public void testDelete() {
+        try {
+            DAO.getEmployee().delete(employee1.getID());
+        } catch (ObjectNotFoundException exception) {
+            throw new RuntimeException(exception);
+        }
+        List<Employee> employeeList = DAO.getEmployee().findMany();
+        int expectedLength = 1;
+        assertEquals(1, employeeList.size());
+        assertEquals(employee2, employeeList.get(0));
+    }
+
+    @Test
+    public void testDeleteMany() {
+        DAO.getEmployee().deleteMany();
+        List<Employee> employeeList = DAO.getEmployee().findMany();
+        assertEquals(0, employeeList.size());
     }
 }
