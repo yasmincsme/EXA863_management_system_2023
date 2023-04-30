eedd8afec7f9bbcb5324dd3fbecd0aae26082295
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-30 18:51:19 -0300

    :sparkles: feat: Adicionando assinatura de novos métodos

--- src/main/java/com/example/exa863_management_system_2023/dao/building/BuildingDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/building/BuildingDAO.java
@@ -3,6 +3,7 @@ package com.example.exa863_management_system_2023.dao.building;
 import com.example.exa863_management_system_2023.dao.CRUD;
 import com.example.exa863_management_system_2023.model.Building;
 import com.example.exa863_management_system_2023.model.Client;
+import com.example.exa863_management_system_2023.model.ComputerComponent;
 
 import java.util.List;
 
@@ -10,5 +11,4 @@ public interface BuildingDAO extends CRUD<Building, Exception> {
     public List<Building> findByWorkOrderID(String workOrderID);
     public double getPriceByServices(String workOrderID);
     public double getCostByServices(String workOrderID);
-
 }
