aa6b6d7e4d72c28546dfe4320e2ee2af728a8acf
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-29 21:25:45 -0300

    :sparkles: feat: Adicionando classe para tratamento dos casos em que o objeto buscado não for encontrado

---
+++ src/main/java/com/example/exa863_management_system_2023/Exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.exa863_management_system_2023.Exceptions;
+
+public class ObjectNotFoundException extends Exception{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
