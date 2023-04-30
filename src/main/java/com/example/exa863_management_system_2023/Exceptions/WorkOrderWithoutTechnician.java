c0906800323ee2440fc8f9185956bd848a05aee2
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-30 20:39:36 -0300

    :sparkles: feat: Adicionando classe para tratamento de erro

---
+++ src/main/java/com/example/exa863_management_system_2023/Exceptions/WorkOrderWithoutTechnician.java
@@ -0,0 +1,8 @@
+package com.example.exa863_management_system_2023.Exceptions;
+
+public class WorkOrderWithoutTechnician extends Exception{
+
+    public WorkOrderWithoutTechnician(String message) {
+        super(message);
+    }
+}
