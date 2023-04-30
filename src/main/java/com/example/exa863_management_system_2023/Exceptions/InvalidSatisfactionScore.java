2492ea91547e4da658d4436897b5af28023ab45b
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-30 20:40:18 -0300

    :sparkles: feat: Adicionando classe para tratamento de erro

---
+++ src/main/java/com/example/exa863_management_system_2023/Exceptions/InvalidSatisfactionScore.java
@@ -0,0 +1,8 @@
+package com.example.exa863_management_system_2023.Exceptions;
+
+public class InvalidSatisfactionScore extends Exception {
+
+    public InvalidSatisfactionScore(String message) {
+        super(message);
+    }
+}
