fb74010f70fbe218723e7db37b187905a7ce48a9
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-04-29 21:27:30 -0300

    :recycle: refactor: Realizando correções de erros apontados nos testes

--- src/main/java/com/example/exa863_management_system_2023/dao/computerComponent/ComponentListImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/computerComponent/ComponentListImplementation.java
@@ -1,5 +1,6 @@
 package com.example.exa863_management_system_2023.dao.computerComponent;
 
+import com.example.exa863_management_system_2023.Exceptions.ObjectNotFoundException;
 import com.example.exa863_management_system_2023.model.ComputerComponent;
 import com.example.exa863_management_system_2023.utils.Generator;
 
@@ -10,28 +11,21 @@ import java.util.UUID;
 public class ComponentListImplementation implements ComputerComponentDAO {
 
     private List<ComputerComponent> stock;
-    private String nextID;
 
     public ComponentListImplementation() {
         this.stock = new ArrayList<ComputerComponent>();
-        this.nextID = Generator.generateID();
     }
 
     @Override
     public ComputerComponent create(ComputerComponent component) {
-        component.setID(this.nextID);
-        this.nextID = Generator.generateID();
+        component.setID(Generator.generateID());
         this.stock.add(component);
         return component;
     }
 
     @Override
     public List<ComputerComponent> findMany() {
-        List<ComputerComponent> computerComponentList = new ArrayList<>();
-        for(ComputerComponent component : this.stock) {
-            computerComponentList.add(component);
-        }
-        return computerComponentList;
+        return stock;
     }
 
     @Override
@@ -56,13 +50,14 @@ public class ComponentListImplementation implements ComputerComponentDAO {
     }
 
     @Override
-    public void update(ComputerComponent component) throws Exception{
+    public void update(ComputerComponent component) throws ObjectNotFoundException {
         for (int i = 0; i < this.stock.size(); i++) {
             if (this.stock.get(i).getID().equals(component.getID())) {
                 this.stock.set(i, component);
                 return;
             }
         }
+        throw new ObjectNotFoundException("The informed component is not registered in the system");
     }
 
     @Override
@@ -78,6 +73,5 @@ public class ComponentListImplementation implements ComputerComponentDAO {
     @Override
     public void deleteMany() {
         this.stock = new ArrayList<>();
-        this.nextID = null;
     }
 }
