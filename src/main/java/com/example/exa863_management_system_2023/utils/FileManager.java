b079a740faa0fb95806cb406c6b410a6e622e192
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-03 13:52:19 -0300

    :goal_net: procedure: Adicionando tratamento de erros

--- src/main/java/com/example/exa863_management_system_2023/utils/FileManager.java
+++ src/main/java/com/example/exa863_management_system_2023/utils/FileManager.java
@@ -1,10 +1,7 @@
 package com.example.exa863_management_system_2023.utils;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager<T> {
@@ -12,10 +9,25 @@ public class FileManager<T> {
     private String fileName;
 
     public FileManager(String fileName) {
-        this.fileName = fileName;
+        if (fileName == null || fileName.trim().isEmpty()) {
+            throw new IllegalArgumentException("File name cannot be null or empty");
+        }
+
+        File file = new File(fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                writeListToFile(new ArrayList<>(), fileName);
+            } catch (IOException exception) {
+                throw new RuntimeException("Unable to create a new file", exception);
+            }
+        } else if (!file.isFile()) {
+            throw new IllegalArgumentException("The provided file name is not a valid file path");
+        }
     }
     public static <T> void writeListToFile(List<T> list, String fileName) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))) {
+            outputStream.write(new byte[0]);
             outputStream.writeObject(list);
         } catch (IOException exception) {
             throw new RuntimeException(exception);
