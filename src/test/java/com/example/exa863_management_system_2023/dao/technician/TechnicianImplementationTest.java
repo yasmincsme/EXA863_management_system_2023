c2bde7e5e6c745e30bc240ce1d4416a32923e354
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-02 23:05:11 -0300

    :recycle: refactor: Alterando nome do teste

--- src/test/java/com/example/exa863_management_system_2023/dao/technician/TechnicianImplementationTest.java
+++ src/test/java/com/example/exa863_management_system_2023/dao/technician/TechnicianImplementationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.*;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TechnicianListImplementationTest {
+public class TechnicianImplementationTest {
     private Technician technician1;
     private Technician technician2;
 
