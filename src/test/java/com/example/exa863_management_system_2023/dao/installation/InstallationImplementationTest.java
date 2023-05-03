a04c1af3f1dc896a0e4c1fb545e11e14720a6434
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-02 23:05:43 -0300

    :recycle: refactor: Alterando nome do teste

--- src/test/java/com/example/exa863_management_system_2023/dao/installation/InstallationImplementationTest.java
+++ src/test/java/com/example/exa863_management_system_2023/dao/installation/InstallationImplementationTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class InstallationListImplementationTest {
+class InstallationImplementationTest {
 
     private Installation installation1;
     private Installation installation2;
