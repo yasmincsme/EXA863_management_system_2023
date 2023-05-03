5a6055ce195bf52365514760b7bc06cf53d61635
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-02 23:10:33 -0300

    :sparkles: feat: Adicionando implementação do "Serializable"

--- src/main/java/com/example/exa863_management_system_2023/model/Technician.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Technician.java
@@ -1,6 +1,8 @@
 package com.example.exa863_management_system_2023.model;
 
-public class Technician {
+import java.io.Serializable;
+
+public class Technician implements Serializable {
 
     private String id;
     private final String type = "TECHNICIAN";
