20b6d77d4a2dc66948562a991e1e38d8c439bb4c
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-04 10:57:18 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/CRUD.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/CRUD.java
@@ -6,15 +6,42 @@ import java.util.List;
 
 public interface CRUD<T, E extends Exception> {
 
+    /**
+     * Cria um novo objeto do tipo especificado.
+     * @param object Objeto que será adicionado ao sistema
+     * @return Objeto recém criado
+     */
     public T create(T object);
 
+    /**
+     * Percorre a lista e retorna o objeto conforme o ID informado.
+     * @param id ID do objeto que se deseja encontrar
+     * @return Objeto desejado
+     */
     public T findByID(String id);
 
+    /**
+     * Retorna todos os objetos do tipo especificado registrados no sistema.
+     * @return Lista com todos os objetos do tipo especificado registrados no sistema
+     */
     public List<T> findMany();
 
+    /**
+     * Percorre a lista e atualiza o objeto informado.
+     * @param object Objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     public void update(T object) throws ObjectNotFoundException;
 
+    /**
+     * Percorre a lista e deleta o objeto informado.
+     * @param id ID do objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     public void delete(String id) throws ObjectNotFoundException;
 
+    /**
+     * Deleta todos os elementos da lista
+     */
     public void deleteMany();
 }
