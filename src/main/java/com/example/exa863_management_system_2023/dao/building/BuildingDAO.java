213a7a890ffc3311bded7ee8c23e36a5f0eb6698
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-04 10:55:13 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/building/BuildingDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/building/BuildingDAO.java
@@ -2,13 +2,29 @@ package com.example.exa863_management_system_2023.dao.building;
 
 import com.example.exa863_management_system_2023.dao.CRUD;
 import com.example.exa863_management_system_2023.model.Building;
-import com.example.exa863_management_system_2023.model.Client;
-import com.example.exa863_management_system_2023.model.ComputerComponent;
 
 import java.util.List;
 
 public interface BuildingDAO extends CRUD<Building, Exception> {
+
+    /**
+     * Percorre a lista e retorna os serviços do tipo "building" que contém o ID da mesma ordem de serviço.
+     * @param workOrderID ID da ordem de serviço
+     * @return Lista com todos os serviços do tipo "building" que pertencem a uma mesma ordem de serviço
+     */
     public List<Building> findByWorkOrderID(String workOrderID);
+
+    /**
+     * Percorre a lista e retorna o preço de todos os serviços do tipo "building" da ordem de serviço desejada.
+     * @param workOrderID ID da ordem de serviço
+     * @return Preço de todos os serviços do tipo "building" de uma ordem de serviço
+     */
     public double getPriceByServices(String workOrderID);
+
+    /**
+     * Percorre a lista e retorna o custo de todos os serviços do tipo "building" da ordem de serviço desejada.
+     * @param workOrderID ID da ordem de serviço
+     * @return Custo de todos os serviços do tipo "building" de uma ordem de serviço
+     */
     public double getCostByServices(String workOrderID);
 }
