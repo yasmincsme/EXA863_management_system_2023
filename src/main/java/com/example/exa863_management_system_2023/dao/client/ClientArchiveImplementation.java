63be9497f79d70d008dd50e831a51f09a97e0cd1
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-07 00:58:24 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/client/ClientArchiveImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientArchiveImplementation.java
@@ -18,6 +18,11 @@ public class ClientArchiveImplementation implements ClientDAO{
         listOfClients = FileManager.readListFromFile("clients.dat");
     }
 
+    /**
+     * Cria um novo objeto do tipo especificado.
+     * @param client Objeto que será adicionado ao sistema
+     * @return Objeto recém criado
+     */
     @Override
     public Client create(Client client) {
         client.setID(Generator.generateID());
@@ -26,11 +31,20 @@ public class ClientArchiveImplementation implements ClientDAO{
         return client;
     }
 
+    /**
+     * Retorna todos os objetos do tipo especificado registrados no sistema.
+     * @return Lista com todos os objetos do tipo especificado registrados no sistema
+     */
     @Override
     public List<Client> findMany() {
         return FileManager.readListFromFile("clients.dat");
     }
 
+    /**
+     * Percorre a lista e retorna o objeto conforme o ID informado.
+     * @param id ID do objeto que se deseja encontrar
+     * @return Objeto desejado
+     */
     @Override
     public Client findByID(String id) {
         for (Client client : this.listOfClients) {
@@ -41,6 +55,11 @@ public class ClientArchiveImplementation implements ClientDAO{
         return null;
     }
 
+    /**
+     * Percorre a lista e retorna o objeto Cliente correspondente ao nome informado.
+     * @param name Nome do Cliente que se deseja encontrar
+     * @return Lista com os clientes que possuem o nome informado
+     */
     @Override
     public List<Client> findByName(String name) {
         List<Client> clientList = new ArrayList<Client>();
@@ -52,6 +71,11 @@ public class ClientArchiveImplementation implements ClientDAO{
         return clientList;
     }
 
+    /**
+     * Percorre a lista e atualiza o objeto informado.
+     * @param client Objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void update(Client client) throws ObjectNotFoundException {
         for (int i = 0; i < listOfClients.size(); i++) {
@@ -64,6 +88,11 @@ public class ClientArchiveImplementation implements ClientDAO{
         throw new ObjectNotFoundException("The informed client is not registered in the system");
     }
 
+    /**
+     * Percorre a lista e deleta o objeto informado.
+     * @param id ID do objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void delete(String id) throws ObjectNotFoundException {
         for (int i = 0; i < this.listOfClients.size(); i++) {
@@ -76,6 +105,9 @@ public class ClientArchiveImplementation implements ClientDAO{
         throw new ObjectNotFoundException("The informed client is not registered in the system");
     }
 
+    /**
+     * Deleta todos os elementos da lista
+     */
     @Override
     public void deleteMany() {
         listOfClients = new ArrayList<>();
