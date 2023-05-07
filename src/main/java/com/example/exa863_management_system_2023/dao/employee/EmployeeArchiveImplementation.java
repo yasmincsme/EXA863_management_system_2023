c27bff8e8a1b51dd42e3fb06d7ce8d88937bda94
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-07 00:59:38 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/employee/EmployeeArchiveImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/employee/EmployeeArchiveImplementation.java
@@ -18,6 +18,11 @@ public class EmployeeArchiveImplementation implements EmployeeDAO {
         listOfEmployees = FileManager.readListFromFile("employees.dat");
     }
 
+    /**
+     * Cria um novo objeto do tipo especificado.
+     * @param employee Objeto que será adicionado ao sistema
+     * @return Objeto recém criado
+     */
     @Override
     public Employee create(Employee employee) {
         employee.setID(Generator.generateID());
@@ -26,11 +31,20 @@ public class EmployeeArchiveImplementation implements EmployeeDAO {
         return employee;
     }
 
+    /**
+     * Retorna todos os objetos do tipo especificado registrados no sistema.
+     * @return Lista com todos os objetos do tipo especificado registrados no sistema
+     */
     @Override
     public List<Employee> findMany() {
         return FileManager.readListFromFile("employees.dat");
     }
 
+    /**
+     * Percorre a lista e retorna o objeto conforme o ID informado.
+     * @param id ID do objeto que se deseja encontrar
+     * @return Objeto desejado
+     */
     @Override
     public Employee findByID(String id) {
         for (Employee employee : this.listOfEmployees) {
@@ -41,6 +55,11 @@ public class EmployeeArchiveImplementation implements EmployeeDAO {
         return null;
     }
 
+    /**
+     * Percorre a lista e retorna o objeto Funcionário correspondente ao login informado.
+     * @param login Login do Funcionário que se deseja encontrar
+     * @return Lista com os funcionários que possuem o login informado
+     */
     @Override
     public List<Employee> findByLogin(String login) {
         List<Employee> employeeList = new ArrayList<>();
@@ -52,6 +71,11 @@ public class EmployeeArchiveImplementation implements EmployeeDAO {
         return employeeList;
     }
 
+    /**
+     * Percorre a lista e atualiza o objeto informado.
+     * @param employee Objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void update(Employee employee) throws ObjectNotFoundException {
         for (int i = 0; i < listOfEmployees.size(); i++) {
@@ -64,6 +88,11 @@ public class EmployeeArchiveImplementation implements EmployeeDAO {
         throw new ObjectNotFoundException("The informed employee is not registered in the system");
     }
 
+    /**
+     * Percorre a lista e deleta o objeto informado.
+     * @param id ID do objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void delete(String id) throws ObjectNotFoundException {
         for (int i = 0; i < this.listOfEmployees.size(); i++) {
@@ -76,6 +105,9 @@ public class EmployeeArchiveImplementation implements EmployeeDAO {
         throw new ObjectNotFoundException("The informed employee is not registered in the system");
     }
 
+    /**
+     * Deleta todos os elementos da lista
+     */
     @Override
     public void deleteMany() {
         listOfEmployees = new ArrayList<>();
