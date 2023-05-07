7f1bf23a22644a86fc4f085a9bb0614abc8d645e
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-06 23:49:23 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/computerComponent/ComputerComponentDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/computerComponent/ComputerComponentDAO.java
@@ -6,5 +6,11 @@ import com.example.exa863_management_system_2023.model.ComputerComponent;
 import java.util.List;
 
 public interface ComputerComponentDAO extends CRUD<ComputerComponent, Exception> {
+
+    /**
+     * Percorre a lista e retorna o objeto Componente correspondente ao nome informado.
+     * @param name Nome do Componente que se deseja encontrar
+     * @return Lista com os componentes que possuem o nome informado
+     */
     public List<ComputerComponent> findByName(String name);
 }
