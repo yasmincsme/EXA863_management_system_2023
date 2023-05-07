183fbf951e6b26337d248d6c955f5aa42cb75791
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-06 23:49:36 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/manager/ManagerDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/manager/ManagerDAO.java
@@ -7,5 +7,11 @@ import com.example.exa863_management_system_2023.model.Manager;
 import java.util.List;
 
 public interface ManagerDAO extends CRUD<Manager, Exception> {
+
+    /**
+     * Percorre a lista e retorna o objeto Técnico correspondente ao login informado.
+     * @param login Login do gerente que se deseja encontrar
+     * @return Lista com os gerentes que possuem o login informado
+     */
     public List<Manager> findByLogin(String login);
 }
