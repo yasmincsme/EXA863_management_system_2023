24d15a26b044ba7c3ccf4f56ed75d3cba982dc61
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-07 00:59:06 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/computerComponent/ComponentListImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/computerComponent/ComponentListImplementation.java
@@ -15,6 +15,11 @@ public class ComponentListImplementation implements ComputerComponentDAO {
         this.stock = new ArrayList<ComputerComponent>();
     }
 
+    /**
+     * Cria um novo objeto do tipo especificado.
+     * @param component Objeto que será adicionado ao sistema
+     * @return Objeto recém criado
+     */
     @Override
     public ComputerComponent create(ComputerComponent component) {
         component.setID(Generator.generateID());
@@ -22,11 +27,20 @@ public class ComponentListImplementation implements ComputerComponentDAO {
         return component;
     }
 
+    /**
+     * Retorna todos os objetos do tipo especificado registrados no sistema.
+     * @return Lista com todos os objetos do tipo especificado registrados no sistema
+     */
     @Override
     public List<ComputerComponent> findMany() {
         return stock;
     }
 
+    /**
+     * Percorre a lista e retorna o objeto conforme o ID informado.
+     * @param id ID do objeto que se deseja encontrar
+     * @return Objeto desejado
+     */
     @Override
     public ComputerComponent findByID(String id) {
         for (ComputerComponent component : this.stock) {
@@ -37,6 +51,11 @@ public class ComponentListImplementation implements ComputerComponentDAO {
         return null;
     }
 
+    /**
+     * Percorre a lista e retorna o objeto Componente correspondente ao nome informado.
+     * @param name Nome do Componente que se deseja encontrar
+     * @return Lista com os componentes que possuem o nome informado
+     */
     @Override
     public List<ComputerComponent> findByName(String name) {
         List<ComputerComponent> componentList = new ArrayList<ComputerComponent>();
@@ -48,6 +67,11 @@ public class ComponentListImplementation implements ComputerComponentDAO {
         return componentList;
     }
 
+    /**
+     * Percorre a lista e atualiza o objeto informado.
+     * @param component Objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void update(ComputerComponent component) throws ObjectNotFoundException {
         for (int i = 0; i < this.stock.size(); i++) {
@@ -59,6 +83,11 @@ public class ComponentListImplementation implements ComputerComponentDAO {
         throw new ObjectNotFoundException("The informed component is not registered in the system");
     }
 
+    /**
+     * Percorre a lista e deleta o objeto informado.
+     * @param id ID do objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void delete(String id) throws ObjectNotFoundException {
         for (int i = 0; i < this.stock.size(); i++) {
@@ -70,6 +99,9 @@ public class ComponentListImplementation implements ComputerComponentDAO {
         throw new ObjectNotFoundException("The informed component is not registered in the system");
     }
 
+    /**
+     * Deleta todos os elementos da lista
+     */
     @Override
     public void deleteMany() {
         this.stock = new ArrayList<>();
