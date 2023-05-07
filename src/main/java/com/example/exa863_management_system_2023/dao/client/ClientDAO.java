f4c5a0451931bac29afbd30990bae54e5a3cd5f7
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-06 23:49:10 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/client/ClientDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientDAO.java
@@ -6,5 +6,11 @@ import com.example.exa863_management_system_2023.model.Client;
 import java.util.List;
 
 public interface ClientDAO extends CRUD<Client, Exception> {
+
+    /**
+     * Percorre a lista e retorna o objeto Cliente correspondente ao nome informado.
+     * @param name Nome do Cliente que se deseja encontrar
+     * @return Lista com os clientes que possuem o nome informado
+     */
     public List<Client> findByName(String name);
 }
