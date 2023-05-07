b1946971e9733cdbb9121df86bf47ef6ae112d0c
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-07 01:01:56 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/technician/TechnicianArchiveImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/technician/TechnicianArchiveImplementation.java
@@ -17,6 +17,11 @@ public class TechnicianArchiveImplementation implements TechnicianDAO {
         listOfTechnicians = FileManager.readListFromFile("technicians.dat");
     }
 
+    /**
+     * Cria um novo objeto do tipo especificado.
+     * @param technician Objeto que será adicionado ao sistema
+     * @return Objeto recém criado
+     */
     @Override
     public Technician create(Technician technician) {
         technician.setID(Generator.generateID());
@@ -25,11 +30,20 @@ public class TechnicianArchiveImplementation implements TechnicianDAO {
         return technician;
     }
 
+    /**
+     * Retorna todos os objetos do tipo especificado registrados no sistema.
+     * @return Lista com todos os objetos do tipo especificado registrados no sistema
+     */
     @Override
     public List<Technician> findMany() {
         return FileManager.readListFromFile("technicians.dat");
     }
 
+    /**
+     * Percorre a lista e retorna o objeto conforme o ID informado.
+     * @param id ID do objeto que se deseja encontrar
+     * @return Objeto desejado
+     */
     @Override
     public Technician findByID(String id) {
         for (Technician technician : this.listOfTechnicians) {
@@ -40,6 +54,11 @@ public class TechnicianArchiveImplementation implements TechnicianDAO {
         return null;
     }
 
+    /**
+     * Percorre a lista e retorna o objeto Técnico correspondente ao nome informado.
+     * @param name Nome do Técnico que se deseja encontrar
+     * @return Lista com os técnicos que possuem o nome informado
+     */
     @Override
     public List<Technician> findByName(String name) {
         List<Technician> technicianList = new ArrayList<>();
@@ -51,6 +70,11 @@ public class TechnicianArchiveImplementation implements TechnicianDAO {
         return technicianList;
     }
 
+    /**
+     * Percorre a lista e atualiza o objeto informado.
+     * @param technician Objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void update(Technician technician) throws ObjectNotFoundException {
         for (int i = 0; i < listOfTechnicians.size(); i++) {
@@ -63,6 +87,11 @@ public class TechnicianArchiveImplementation implements TechnicianDAO {
         throw new ObjectNotFoundException("The informed technician is not registered in the system");
     }
 
+    /**
+     * Percorre a lista e deleta o objeto informado.
+     * @param id ID do objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void delete(String id) throws ObjectNotFoundException {
         for (int i = 0; i < this.listOfTechnicians.size(); i++) {
@@ -75,6 +104,9 @@ public class TechnicianArchiveImplementation implements TechnicianDAO {
         throw new ObjectNotFoundException("The informed technician is not registered in the system");
     }
 
+    /**
+     * Deleta todos os elementos da lista
+     */
     @Override
     public void deleteMany() {
         listOfTechnicians = new ArrayList<>();
