9f02e28acdff9a0421cd6fec704337f4e23cb7ff
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-07 00:58:37 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/client/ClientListImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientListImplementation.java
@@ -1,6 +1,8 @@
 package com.example.exa863_management_system_2023.dao.client;
 
+import com.example.exa863_management_system_2023.Exceptions.ObjectNotFoundException;
 import com.example.exa863_management_system_2023.model.Client;
+import com.example.exa863_management_system_2023.utils.FileManager;
 import com.example.exa863_management_system_2023.utils.Generator;
 
 import java.util.ArrayList;
@@ -14,6 +16,11 @@ public class ClientListImplementation implements ClientDAO {
         this.listOfClients = new ArrayList<Client>();
     }
 
+    /**
+     * Cria um novo objeto do tipo especificado.
+     * @param client Objeto que será adicionado ao sistema
+     * @return Objeto recém criado
+     */
     @Override
     public Client create(Client client) {
         client.setID(Generator.generateID());
@@ -21,11 +28,20 @@ public class ClientListImplementation implements ClientDAO {
         return client;
     }
 
+    /**
+     * Retorna todos os objetos do tipo especificado registrados no sistema.
+     * @return Lista com todos os objetos do tipo especificado registrados no sistema
+     */
     @Override
     public List<Client> findMany() {
         return listOfClients;
     }
 
+    /**
+     * Percorre a lista e retorna o objeto conforme o ID informado.
+     * @param id ID do objeto que se deseja encontrar
+     * @return Objeto desejado
+     */
     @Override
     public Client findByID(String id) {
         for (Client client : this.listOfClients) {
@@ -36,6 +52,11 @@ public class ClientListImplementation implements ClientDAO {
         return null;
     }
 
+    /**
+     * Percorre a lista e retorna o objeto Cliente correspondente ao nome informado.
+     * @param name Nome do Cliente que se deseja encontrar
+     * @return Lista com os clientes que possuem o nome informado
+     */
     @Override
     public List<Client> findByName(String name) {
         List<Client> clientList = new ArrayList<Client>();
@@ -47,26 +68,43 @@ public class ClientListImplementation implements ClientDAO {
         return clientList;
     }
 
+    /**
+     * Percorre a lista e atualiza o objeto informado.
+     * @param client Objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
-    public void update(Client client) {
-        for (int i = 0; i < this.listOfClients.size(); i++) {
-            if (this.listOfClients.get(i).getID().equals(client.getID())) {
-                this.listOfClients.set(i, client);
+    public void update(Client client) throws ObjectNotFoundException {
+        for (int i = 0; i < listOfClients.size(); i++) {
+            if (listOfClients.get(i).getID().equals(client.getID())) {
+                listOfClients.set(i, client);
+                FileManager.writeListToFile(listOfClients, "clients.dat");
                 return;
             }
         }
+        throw new ObjectNotFoundException("The informed client is not registered in the system");
     }
 
+    /**
+     * Percorre a lista e deleta o objeto informado.
+     * @param id ID do objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
-    public void delete(String id) {
+    public void delete(String id) throws ObjectNotFoundException {
         for (int i = 0; i < this.listOfClients.size(); i++) {
-            if (this.listOfClients.get(i).getID().equals(id)) {
-                this.listOfClients.remove(i);
+            if (listOfClients.get(i).getID().equals(id)) {
+                listOfClients.remove(i);
+                FileManager.writeListToFile(listOfClients, "clients.dat");
                 return;
             }
         }
+        throw new ObjectNotFoundException("The informed client is not registered in the system");
     }
 
+    /**
+     * Deleta todos os elementos da lista
+     */
     @Override
     public void deleteMany() {
         this.listOfClients = new ArrayList<>();
