1adf278d9a8d698fbb3c068b4e0a2c88d7c8b785
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-07 00:59:53 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/employee/EmployeeDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/employee/EmployeeDAO.java
@@ -6,5 +6,11 @@ import com.example.exa863_management_system_2023.model.Employee;
 import java.util.List;
 
 public interface EmployeeDAO extends CRUD<Employee, Exception> {
+
+    /**
+     * Percorre a lista e retorna o objeto Funcionário correspondente ao login informado.
+     * @param login Login do Funcionário que se deseja encontrar
+     * @return Lista com os funcionários que possuem o login informado
+     */
     public List<Employee> findByLogin(String login);
 }
