100d33456935612233b7442b2831c0801584fc6b
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-07 01:02:52 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/workOrder/WorkOrderArchiveImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/workOrder/WorkOrderArchiveImplementation.java
@@ -19,6 +19,11 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         listOfWorkOrder = FileManager.readListFromFile("orders.dat");
     }
 
+    /**
+     * Cria um novo objeto do tipo especificado.
+     * @param workOrder Objeto que será adicionado ao sistema
+     * @return Objeto recém criado
+     */
     @Override
     public WorkOrder create(WorkOrder workOrder) {
         workOrder.setID(Generator.generateID());
@@ -27,11 +32,20 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         return workOrder;
     }
 
+    /**
+     * Retorna todos os objetos do tipo especificado registrados no sistema.
+     * @return Lista com todos os objetos do tipo especificado registrados no sistema
+     */
     @Override
     public List<WorkOrder> findMany() {
         return FileManager.readListFromFile("orders.dat");
     }
 
+    /**
+     * Percorre a lista e retorna o objeto conforme o ID informado.
+     * @param id ID do objeto que se deseja encontrar
+     * @return Objeto desejado
+     */
     @Override
     public WorkOrder findByID(String id) {
         for (WorkOrder workOrder : this.listOfWorkOrder) {
@@ -42,6 +56,10 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         return null;
     }
 
+    /**
+     * Percorre a lista e retorna as ordens de serviço que estão em aberto.
+     * @return Lista com todas as ordens de serviço em aberto
+     */
     @Override
     public List<WorkOrder> findOpenWorkOrders() {
         List<WorkOrder> openWorkOrders = new ArrayList<WorkOrder>();
@@ -53,6 +71,10 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         return openWorkOrders;
     }
 
+    /**
+     * Percorre a lista e retorna a primeira ordem de serviço em aberto da lista.
+     * @return Primeira ordem de serviço em aberto da lista
+     */
     public WorkOrder getFirstOpenedWorkOrder() {
         List<WorkOrder> openWorkOrders = new ArrayList<>();
         for (WorkOrder workOrder : this.listOfWorkOrder) {
@@ -63,6 +85,11 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         return null;
     }
 
+    /**
+     * Percorre a lista e retorna uma lista com todas as ordens de serviço que pertencem a um mesmo cliente.
+     * @param clientID ID do cliente desejado
+     * @return Lista com todas as ordens de serviço que pertencem a um mesmo cliente
+     */
     @Override
     public List<WorkOrder> findOrderByClientID(String clientID) {
         List<WorkOrder> workOrderListByClient = new ArrayList<>();
@@ -74,6 +101,11 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         return workOrderListByClient;
     }
 
+    /**
+     * Percorre a lista e retorna uma lista com todas as ordens de serviço que já foram designadas a um mesmo técnico.
+     * @param technicianID ID do técnico desejado
+     * @return Lista com todas as ordens de serviço que pertencem a um mesmo técnico
+     */
     @Override
     public List<WorkOrder> findOrderByTechnicianID(String technicianID) {
         List<WorkOrder> workOrderListByTechnician = new ArrayList<>();
@@ -85,6 +117,11 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         return workOrderListByTechnician;
     }
 
+    /**
+     * Percorre a lista e atualiza o objeto informado.
+     * @param workOrder Objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void update(WorkOrder workOrder) throws ObjectNotFoundException {
         for (int i = 0; i < listOfWorkOrder.size(); i++) {
@@ -97,6 +134,11 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         throw new ObjectNotFoundException("The informed work order is not registered in the system");
     }
 
+    /**
+     * Percorre a lista e deleta o objeto informado.
+     * @param id ID do objeto que será atualizado
+     * @throws ObjectNotFoundException
+     */
     @Override
     public void delete(String id) throws ObjectNotFoundException{
         for (int i = 0; i < this.listOfWorkOrder.size(); i++) {
@@ -109,18 +151,31 @@ public class WorkOrderArchiveImplementation implements WorkOrderDAO {
         throw new ObjectNotFoundException("The informed work order is not registered in the system");
     }
 
+    /**
+     * Deleta todos os elementos da lista
+     */
     @Override
     public void deleteMany() {
         listOfWorkOrder = new ArrayList<>();
         FileManager.writeListToFile(listOfWorkOrder, "orders.dat");
     }
 
+    /**
+     * Procura no sistema por todos os serviços pertencentes a uma mesma ordem de serviço e retorna o preço final.
+     * @param workOrderID ID da ordem de serviço
+     * @return Preço final de todos os serviços de uma ordem de serviço
+     */
     @Override
     public double getPriceByWorkOrderID(String workOrderID) {
         return DAO.getBuilding().getPriceByServices(workOrderID) + DAO.getCleaning().getPriceByServices(workOrderID) + DAO.getInstallation().getPriceByServices(workOrderID);
 
     }
 
+    /**
+     * Procura no sistema por todos os serviços pertencentes a uma mesma ordem de serviço e retorna o custo final.
+     * @param workOrderID ID da ordem de serviço
+     * @return Preço final de todos os serviços de uma ordem de serviço
+     */
     @Override
     public double getCostByWorkOrderID(String workOrderID) {
         return DAO.getBuilding().getCostByServices(workOrderID) + DAO.getCleaning().getCostByServices(workOrderID) + DAO.getInstallation().getCostByServices(workOrderID);
