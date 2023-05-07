d5a3247f2f0274045dec7a55b2ea69bfcb087e6c
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-06 23:49:50 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/dao/technician/TechnicianDAO.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/technician/TechnicianDAO.java
@@ -6,5 +6,11 @@ import com.example.exa863_management_system_2023.model.Technician;
 import java.util.List;
 
 public interface TechnicianDAO extends CRUD<Technician, Exception> {
+
+    /**
+     * Percorre a lista e retorna o objeto Técnico correspondente ao nome informado.
+     * @param name Nome do Técnico que se deseja encontrar
+     * @return Lista com os técnicos que possuem o nome informado
+     */
     public List<Technician> findByName(String name);
 }
