5f129da6b12002a0b0885e7c9bc7b2a9eaf4586a
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-08 22:31:56 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/model/Installation.java
+++ src/main/java/com/example/exa863_management_system_2023/model/Installation.java
@@ -14,12 +14,12 @@ public class Installation implements Serializable {
     private double cost;
     private String operatingSystem;
 
+
     /**
-     *
-     * @param description Description of the Installation
-     * @param price Price of the Installation
-     * @param cost Cost of the Installation
-     * @param operatingSystem Hardware's operating system
+     * Cria um novo objeto do tipo "installation".
+     * @param description Descrição do serviço
+     * @param price Preço do serviço
+     * @param cost Custo do serviço
      */
     public Installation(String description, String workOrderID, double price, double cost, String operatingSystem) {
         this.id = null;
@@ -31,103 +31,103 @@ public class Installation implements Serializable {
     }
 
     /**
-     *
-     * @return Return Service's ID
+     * Retorna o ID do objeto.
+     * @return ID do objeto
      */
     public String getID() { return id;}
 
     /**
-     *
-     * @param id New value to ID
+     * Atribui um novo valor para o ID.
+     * @param id Novo valor para o ID
      */
     public void setID(String id) {
         this.id = id;
     }
 
     /**
-     *
-     * @return ID of the work order that it belongs
+     * Retorna o ID da ordem de serviço a qual o serviço está associado.
+     * @return ID da ordem de serviço a qual o serviço está associado
      */
     public String getWorkOrderID() {
         return workOrderID;
     }
 
     /**
-     *
-     * @param workOrderID New value to work order's ID
+     * Atribui um novo valor para o ID da ordem de serviço a qual o serviço está associado.
+     * @param workOrderID Novo valor para o ID da ordem de serviço a qual o serviço está associado
      */
     public void setWorkOrderID(String workOrderID) {
         this.workOrderID = workOrderID;
     }
 
     /**
-     *
-     * @return Return Service's description
+     * Retorna a descrição do serviço.
+     * @return Descrição do serviço
      */
     public String getDescription() {
         return description;
     }
 
     /**
-     *
-     * @param description New value to description
+     * Atribui uma nova informação à descrição.
+     * @param description Nova descrição para o serviço
      */
     public void setDescription(String description) {
         this.description = description;
     }
 
     /**
-     *
-     * @return Return Service's price
+     * Retorna o preço do serviço.
+     * @return Preço do serviço
      */
     public double getPrice() {
         return price;
     }
 
     /**
-     *
-     * @param price New value to price
+     * Atribui um novo valor para o preço do serviço.
+     * @param price Novo preço para o serviço
      */
     public void setPrice(double price) {
         this.price = price;
     }
 
     /**
-     *
-     * @return Return Service's cost
+     * Retorna o custo do serviço.
+     * @return Custo do serviço
      */
     public double getCost() {
         return cost;
     }
 
     /**
-     *
-     * @param cost New value to cost
+     * Atribui um novo valor para o custo do serviço.
+     * @param cost Novo custo para o serviço
      */
     public void setCost(double cost) {
         this.cost = cost;
     }
 
     /**
-     *
-     * @return Return Installation's operating system
+     * Retorna o sistema operacional do sistema do computador do cliente.
+     * @return Sistema operacional do sistema do computador do cliente
      */
     public String getOperatingSystem() {
         return operatingSystem;
     }
 
     /**
-     *
-     * @param operatingSystem New value to operating System
+     *Aribui uma nova informação sobre o sistema operacional que substitui a anterior.
+     * @param operatingSystem Nova informação sobre o sistema operacional
      */
     public void setOperatingSystem(String operatingSystem) {
         this.operatingSystem = operatingSystem;
     }
 
     /**
-     *
-     * @param object Receive a random object
-     * @return Return true if the given object is equal to the current Installation object, or false if they are not the same
+     * Compara dois objetos do tipo "installation" a partir do ID
+     * @param object Objeto do tipo "installation"
+     * @return True se o objeto informado tem o mesmo ID do objeto comparado, False caso contrário.
      */
     @Override
     public boolean equals(Object object) {
@@ -141,8 +141,8 @@ public class Installation implements Serializable {
     }
 
     /**
-     *
-     * @return Return a short formatted description of the object
+     * Retorna uma representação em String do objeto.
+     * @return Representação em String do objeto
      */
     @Override
     public String toString() {
