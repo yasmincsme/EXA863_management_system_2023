ad07dc2283c30f1bf3ec0b34904d9b1f5ff1b4f4
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-08 22:32:06 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/Exceptions/InvalidSatisfactionScore.java
+++ src/main/java/com/example/exa863_management_system_2023/Exceptions/InvalidSatisfactionScore.java
@@ -2,6 +2,10 @@ package com.example.exa863_management_system_2023.Exceptions;
 
 public class InvalidSatisfactionScore extends Exception {
 
+    /**
+     * Uma determinada mensagem é lançada quando um valor inválido é informado
+     * @param message Mensagem de erro
+     */
     public InvalidSatisfactionScore(String message) {
         super(message);
     }
