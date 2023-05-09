50e600ceffe34bdc82831546e24cef462d0e2d39
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-08 22:32:34 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/Exceptions/ObjectNotFoundException.java
+++ src/main/java/com/example/exa863_management_system_2023/Exceptions/ObjectNotFoundException.java
@@ -2,6 +2,10 @@ package com.example.exa863_management_system_2023.Exceptions;
 
 public class ObjectNotFoundException extends Exception{
 
+    /**
+     * Uma determinada mensagem é lançada quando o objeto não é encontrado
+     * @param message Mensagem de erro
+     */
     public ObjectNotFoundException(String message) {
         super(message);
     }
