316862604aa42c6ad3bb38379c2e7d017985a2f4
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-05-08 22:33:09 -0300

    :bulb: docs: Adicionando comentários para documentação

--- src/main/java/com/example/exa863_management_system_2023/Exceptions/WorkOrderWithoutTechnician.java
+++ src/main/java/com/example/exa863_management_system_2023/Exceptions/WorkOrderWithoutTechnician.java
@@ -2,6 +2,10 @@ package com.example.exa863_management_system_2023.Exceptions;
 
 public class WorkOrderWithoutTechnician extends Exception{
 
+    /**
+     * Uma determinada mensagem é lançada quando uma ordem de serviço é finalizada sem um técnico
+     * @param message Mensagem de erro
+     */
     public WorkOrderWithoutTechnician(String message) {
         super(message);
     }
