0efbab22294101018717adb6b716e14ccec1ea1f
Rafael Tosta <rafa.ecomp@gmail.com>
2023-05-12 09:06:18 -0300

    :sparkles: feat: Suite de teste
    Testa todos módulos de um única vez.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,18 @@ module com.example.exa863_management_system_2023 {
 
     requires org.kordamp.bootstrapfx.core;
 
+    opens com.example.exa863_management_system_2023.model;
+
+    opens com.example.exa863_management_system_2023.dao.manager;
+    opens com.example.exa863_management_system_2023.dao.building;
+    opens com.example.exa863_management_system_2023.dao.cleaning;
+    opens com.example.exa863_management_system_2023.dao.client;
+    opens com.example.exa863_management_system_2023.dao.computerComponent;
+    opens com.example.exa863_management_system_2023.dao.employee;
+    opens com.example.exa863_management_system_2023.dao.installation;
+    opens com.example.exa863_management_system_2023.dao.technician;
+    opens com.example.exa863_management_system_2023.dao.workOrder;
+
     opens com.example.exa863_management_system_2023 to javafx.fxml;
     exports com.example.exa863_management_system_2023;
 }
