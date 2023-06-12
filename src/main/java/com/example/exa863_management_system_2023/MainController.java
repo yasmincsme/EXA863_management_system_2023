ec9fc624734d73812f4ff82a2d356484340fd05a
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-12 01:32:28 -0300

    :sparkles: feat: Controller para main

---
+++ src/main/java/com/example/exa863_management_system_2023/MainController.java
@@ -0,0 +1,16 @@
+package com.example.exa863_management_system_2023;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
