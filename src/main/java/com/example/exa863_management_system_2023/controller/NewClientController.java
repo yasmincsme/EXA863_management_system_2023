621bebc9b0f6db321d8ac48e1e1f0bc9796c46e7
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-12 01:29:31 -0300

    :sparkles: feat: Controller para new client

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewClientController.java
@@ -0,0 +1,27 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewClientController {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private Button saveButton;
+
+}
