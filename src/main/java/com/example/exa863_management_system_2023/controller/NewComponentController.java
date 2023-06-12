2244a97da2db015da204ba7ab673d5e8d290357c
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-12 01:29:57 -0300

    :sparkles: feat: Controller para new component controller

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewComponentController.java
@@ -0,0 +1,33 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewComponentController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField manufacturerField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField serialNumberField;
+
+    @FXML
+    private TextField unitCostField;
+
+    @FXML
+    private TextField unitPriceField;
+
+}
