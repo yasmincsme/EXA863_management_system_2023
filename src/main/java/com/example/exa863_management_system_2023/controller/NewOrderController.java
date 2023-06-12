40eb646b30041c18e9f5e7da0f2b49d7714e0830
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-12 01:30:38 -0300

    :sparkles: feat: Controller para new order controller

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewOrderController.java
@@ -0,0 +1,36 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewOrderController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField clientIDField;
+
+    @FXML
+    private TextField costField;
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private TextField paymentMethodField;
+
+    @FXML
+    private TextField priceField;
+
+    @FXML
+    private TextField satisfactionScoreField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField technicianIDField;
+
+}
