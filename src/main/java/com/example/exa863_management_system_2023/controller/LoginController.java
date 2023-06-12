e5049245961f0fbb79568cc042ae8030435e101f
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-12 01:29:11 -0300

    :sparkles: feat: Controller para login

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void toLogin(ActionEvent event) {
+
+    }
+
+}
+
+
