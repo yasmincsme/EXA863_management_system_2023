07ed33048daa3e3880a2f4db028e77b05e1ee01c
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-12 01:32:11 -0300

    :sparkles: feat: Controller para new users

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewUserController.java
@@ -0,0 +1,24 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewUserController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField roleField;
+
+    @FXML
+    private Button saveButton;
+
+}
