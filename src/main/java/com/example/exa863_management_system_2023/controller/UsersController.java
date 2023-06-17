6b06249e639a546c44da4694e207fe2e9d72e2aa
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:54:33 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/controller/UsersController.java
+++ src/main/java/com/example/exa863_management_system_2023/controller/UsersController.java
@@ -1,70 +1,178 @@
 package com.example.exa863_management_system_2023.controller;
 
+import com.example.exa863_management_system_2023.MainController;
+import com.example.exa863_management_system_2023.dao.DAO;
+import com.example.exa863_management_system_2023.exceptions.ObjectNotFoundException;
+import com.example.exa863_management_system_2023.model.Employee;
+import com.example.exa863_management_system_2023.model.Manager;
+import com.example.exa863_management_system_2023.model.Technician;
+import com.example.exa863_management_system_2023.utils.FindUsers;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.Stage;
 
-public class UsersController {
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class UsersController extends MenuController {
+
+    //Elementos das colunas
     @FXML
-    private Label IDLabel;
-
+    private TableView<Object> usersTable;
     @FXML
-    private Button clientsButton;
-
+    private TableColumn<Object, String> nameColumn;
     @FXML
-    private TableColumn<?, ?> emailColumn;
-
+    private TableColumn<Object, String> emailColumn;
     @FXML
-    private Label emailLabel;
+    private TableColumn<Object, String> roleColumn;
 
-    @FXML
-    private TableColumn<?, ?> nameColumn;
 
+    //Label
     @FXML
     private Label nameLabel;
-
     @FXML
-    private Button newUserButton;
-
+    private Label IDLabel;
     @FXML
-    private Button ordersButton;
-
-    @FXML
-    private Button overviewButton;
-
-    @FXML
-    private Button removeButton;
-
-    @FXML
-    private TableColumn<?, ?> roleColumn;
-
+    private Label emailLabel;
     @FXML
     private Label roleLabel;
 
+
+    //Button
     @FXML
-    private TextField searchField;
+    private Button removeButton;
+    @FXML
+    private Button updateUserButton;
+    @FXML
+    private Button newUserButton;
+
+
+    public static Object selectedUser;
+
 
     @FXML
-    private Button settingsButton;
+    private void initialize(){
+        selectedUser = null;
+        initializeTable();
+    }
+
+    private void initializeTable(){
+
+        //Valores para o nome
+        this.nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        //Valores para o email
+        this.emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        this.emailColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        //Valores para o tipo
+        this.roleColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        this.roleColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.usersTable.onMouseClickedProperty().setValue(mouseEvent -> {
+            if (usersTable.getSelectionModel().getSelectedItem() != null) {
+                UsersController.selectedUser = usersTable.getSelectionModel().getSelectedItem();
+                this.showUser();
+            }
+        });
+
+        ArrayList<Object> usersList = FindUsers.findAllUsers();
+        for (Object user : usersList) {
+            usersTable.getItems().add(user);
+        }
+    }
+
+    private void showUser(){
+        if (UsersController.selectedUser != null) {
+            Manager newUser = null;
+            //mostrar o usuario
+            if (UsersController.selectedUser instanceof Manager) {
+                newUser = (Manager) UsersController.selectedUser;
+                this.nameLabel.setText(newUser.getName());
+                this.IDLabel.setText(newUser.getID());
+                this.emailLabel.setText(newUser.getEmail());
+                this.roleLabel.setText(newUser.getType());
+            }
+        } else {
+            this.nameLabel.setText("name");
+            this.IDLabel.setText("ID");
+            this.emailLabel.setText("email");
+            this.roleLabel.setText("role");
+        }
+        if (UsersController.selectedUser != null) {
+            Technician newUser = null;
+            //mostrar o usuario
+            if (UsersController.selectedUser instanceof Technician) {
+                newUser = (Technician) UsersController.selectedUser;
+                this.nameLabel.setText(newUser.getName());
+                this.IDLabel.setText(newUser.getID());
+                this.emailLabel.setText(newUser.getEmail());
+                this.roleLabel.setText(newUser.getType());
+            }
+        } else {
+            this.nameLabel.setText("name");
+            this.IDLabel.setText("ID");
+            this.emailLabel.setText("email");
+            this.roleLabel.setText("role");
+        }
+        if (UsersController.selectedUser != null) {
+            Employee newUser = null;
+            //mostrar o usuario
+            if (UsersController.selectedUser instanceof Employee) {
+                newUser = (Employee) UsersController.selectedUser;
+                this.nameLabel.setText(newUser.getName());
+                this.IDLabel.setText(newUser.getID());
+                this.emailLabel.setText(newUser.getEmail());
+                this.roleLabel.setText(newUser.getType());
+            }
+        } else {
+            this.nameLabel.setText("name");
+            this.IDLabel.setText("ID");
+            this.emailLabel.setText("email");
+            this.roleLabel.setText("role");
+        }
+    }
 
     @FXML
-    private Button signoutButton;
+    void removeUser(ActionEvent event) throws IOException {
+        if (selectedUser instanceof Manager){
+            try {
+                DAO.getManager().delete(((Manager) selectedUser).getID());
+            } catch (ObjectNotFoundException exception) {
+                throw new RuntimeException(exception);
+            }
+        } else if (selectedUser instanceof Technician) {
+            try {
+                DAO.getTechnician().delete(((Technician) selectedUser).getID());
+            } catch (ObjectNotFoundException exception) {
+            }
+        } else if (selectedUser instanceof Employee) {
+            try {
+                DAO.getEmployee().delete(((Employee) selectedUser).getID());
+            } catch (ObjectNotFoundException exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+        MainController.gotoScene("UsersView.fxml");
+    }
 
     @FXML
-    private Button stockButton;
+    void updateUser(ActionEvent event) throws IOException {
+        MainController.popUp("UpdateUserView.fxml");
+    }
 
     @FXML
-    private Label usernameLabel;
-
-    @FXML
-    private Button usersButton;
-
-    @FXML
-    private TableView<?> usersTable;
-
+    void addNewUser(ActionEvent event) throws IOException {
+        Stage popUpStage = MainController.popUp("NewUserView.fxml");
+    }
 }
+
 
