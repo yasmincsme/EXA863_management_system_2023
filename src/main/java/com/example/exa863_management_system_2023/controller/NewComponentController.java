bf763f2e84c351135963191282c5edddcb6b5030
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:55:55 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/controller/NewComponentController.java
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewComponentController.java
@@ -1,5 +1,8 @@
 package com.example.exa863_management_system_2023.controller;
 
+import com.example.exa863_management_system_2023.dao.DAO;
+import com.example.exa863_management_system_2023.model.ComputerComponent;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -29,5 +32,19 @@ public class NewComponentController {
 
     @FXML
     private TextField unitPriceField;
+
+    @FXML
+    void cancelOperation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveComponent(ActionEvent event) {
+        double unitPrice = Double.parseDouble(unitPriceField.getText());
+        double unitCost =  Double.parseDouble(unitCostField.getText());
+        int quantity = Integer.parseInt(unitCostField.getText());
+        ComputerComponent component = new ComputerComponent(nameField.getText(), manufacturerField.getText(), serialNumberField.getText(), unitPrice, unitCost, quantity);
+        DAO.getComponent().create(component);
+    }
 
 }
