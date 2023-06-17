bda5314bc87529161839b16a420e2c7173e75ab4
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:56:17 -0300

    :sparkles: feat: Novo controller

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/UpdateClientController.java
@@ -0,0 +1,41 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UpdateClientController {
+
+    @FXML
+    private TextField IDField;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    void cancelOperation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveClient(ActionEvent event) {
+
+    }
+
+}
