529869b91903744d0021e25f09ca8486419dbc63
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:59:01 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/controller/ClientsController.java
+++ src/main/java/com/example/exa863_management_system_2023/controller/ClientsController.java
@@ -1,69 +1,123 @@
 package com.example.exa863_management_system_2023.controller;
 
+import com.example.exa863_management_system_2023.MainController;
+import com.example.exa863_management_system_2023.dao.DAO;
+import com.example.exa863_management_system_2023.exceptions.ObjectNotFoundException;
+import com.example.exa863_management_system_2023.model.Cleaning;
+import com.example.exa863_management_system_2023.model.Client;
+import com.example.exa863_management_system_2023.utils.FindUsers;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
-public class ClientsController {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ClientsController extends MenuController{
+
+    //Elementos das colunas
     @FXML
-    private Label IDLabel;
-
+    private TableView<Client> usersTable;
     @FXML
-    private Label addressLabel;
-
+    private TableColumn<Client, String> nameColumn;
     @FXML
-    private Button clientsButton;
+    private TableColumn<Client, String> emailColumn;
 
-    @FXML
-    private TableView<?> clientsTable;
-
-    @FXML
-    private TableColumn<?, ?> emailColumn;
-
-    @FXML
-    private Label emailLabel;
-
-    @FXML
-    private TableColumn<?, ?> nameColumn;
-
+    //Label
     @FXML
     private Label nameLabel;
-
     @FXML
-    private Button newUserButton;
-
+    private Label IDLabel;
     @FXML
-    private Button ordersButton;
-
+    private Label emailLabel;
     @FXML
-    private Button overviewButton;
-
+    private Label addressLabel;
     @FXML
     private Label phoneLabel;
 
+    //Button
     @FXML
     private Button removeButton;
+    @FXML
+    private Button updateClientButton;
+    @FXML
+    private Button newClientButton;
+
+    public static Object selectedUser;
+
 
     @FXML
-    private TextField searchField;
+    private void initialize(){
+        selectedUser = null;
+        initializeTable();
+    }
+
+    private void initializeTable(){
+
+        //Valores para o nome
+        this.nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        //Valores para o email
+        this.emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        this.emailColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.usersTable.onMouseClickedProperty().setValue(mouseEvent -> {
+            if (usersTable.getSelectionModel().getSelectedItem() != null) {
+                ClientsController.selectedUser = usersTable.getSelectionModel().getSelectedItem();
+                this.showUser();
+            }
+        });
+
+        List<Client> clientList = DAO.getClient().findMany();
+        for (Client client : clientList) {
+            usersTable.getItems().add(client);
+        }
+    }
+
+    private void showUser() {
+        if (ClientsController.selectedUser != null) {
+            Client newClient = null;
+            newClient = (Client) ClientsController.selectedUser;
+            this.nameLabel.setText(newClient.getName());
+            this.IDLabel.setText((newClient.getID()));
+            this.emailLabel.setText(newClient.getEmail());
+            this.addressLabel.setText(newClient.getAddress());
+            this.phoneLabel.setText(newClient.getPhone());
+        } else {
+            this.nameLabel.setText("name");
+            this.IDLabel.setText("ID");
+            this.emailLabel.setText("email");
+            this.addressLabel.setText("address");
+            this.phoneLabel.setText("phone");
+        }
+
+    }
 
     @FXML
-    private Button settingsButton;
+    void removeClient(ActionEvent event) throws IOException{
+        try {
+            DAO.getClient().delete(((Client) selectedUser).getID());
+        } catch (ObjectNotFoundException exception) {
+            throw new RuntimeException(exception);
+        }
+        MainController.gotoScene("ClientView.fxml");
+    }
 
     @FXML
-    private Button signoutButton;
+    void updateClient(ActionEvent event) {
+
+    }
 
     @FXML
-    private Button stockButton;
+    void addNewClient(ActionEvent event) {
 
-    @FXML
-    private Label usernameLabel;
-
-    @FXML
-    private Button usersButton;
-
+    }
 }
