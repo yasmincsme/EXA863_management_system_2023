6b8204adc8a7f8baf3e5c759de682edc19667cbc
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:55:15 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/MainController.java
+++ src/main/java/com/example/exa863_management_system_2023/MainController.java
@@ -1,7 +1,17 @@
 package com.example.exa863_management_system_2023;
 
+import com.example.exa863_management_system_2023.dao.DAO;
+import com.example.exa863_management_system_2023.model.Employee;
+import com.example.exa863_management_system_2023.model.Manager;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainController extends Application {
 
@@ -10,7 +20,12 @@ public class MainController extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        MainController.stage = new Stage();
+        Manager manager = new Manager("Admin", "admin@example", "admin", "admin123");
+        if (DAO.getManager().findByLogin("yamisan").size() == 0) {
+            DAO.getManager().create(manager);
+        }
+        gotoScene("LoginView.fxml");
     }
 }
