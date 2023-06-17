4747aaf7065fa52e1991a2a3d3055eba2d2fa44d
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:56:31 -0300

    :sparkles: feat: Novo controller

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/UpdateComponentController.java
@@ -0,0 +1,47 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UpdateComponentController {
+
+    @FXML
+    private TextField IDField;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField manufacturerField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField serialNumberField;
+
+    @FXML
+    private TextField unitCostField;
+
+    @FXML
+    private TextField unitPriceField;
+
+    @FXML
+    void cancelOperation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveComponent(ActionEvent event) {
+
+    }
+
+}
