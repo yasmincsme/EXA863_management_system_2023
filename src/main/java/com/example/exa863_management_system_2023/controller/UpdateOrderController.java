2bd01d731161441cce9a96501d8960e726903fa8
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:56:51 -0300

    :sparkles: feat: Novo controller

---
+++ src/main/java/com/example/exa863_management_system_2023/controller/UpdateOrderController.java
@@ -0,0 +1,50 @@
+package com.example.exa863_management_system_2023.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UpdateOrderController {
+
+    @FXML
+    private TextField IDField;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField clientIDField;
+
+    @FXML
+    private TextField costField;
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private TextField paymentMethodField;
+
+    @FXML
+    private TextField priceField;
+
+    @FXML
+    private TextField satisfactionScoreField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField technicianID;
+
+    @FXML
+    void cancelOperation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveOrder(ActionEvent event) {
+
+    }
+
+}
