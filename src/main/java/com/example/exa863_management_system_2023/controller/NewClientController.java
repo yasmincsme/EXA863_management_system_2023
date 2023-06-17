2d3dd9ddf01ed3e57657ee7dc0f95f0075f469c6
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-17 20:54:48 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/controller/NewClientController.java
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewClientController.java
@@ -1,5 +1,8 @@
 package com.example.exa863_management_system_2023.controller;
 
+import com.example.exa863_management_system_2023.dao.DAO;
+import com.example.exa863_management_system_2023.model.Client;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -23,5 +26,16 @@ public class NewClientController {
 
     @FXML
     private Button saveButton;
+
+    @FXML
+    void cancelOperation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveClient(ActionEvent event) {
+        Client client = new Client(nameField.getText(), emailField.getText(), addressField.getText(), phoneField.getText());
+        DAO.getClient().create(client);
+    }
 
 }
