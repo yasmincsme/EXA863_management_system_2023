bad8c7fd1643b5802e55c627cc5e62a169c07985
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-18 00:59:41 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/controller/NewComponentController.java
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewComponentController.java
@@ -1,50 +1,58 @@
 package com.example.exa863_management_system_2023.controller;
 
+import com.example.exa863_management_system_2023.MainController;
 import com.example.exa863_management_system_2023.dao.DAO;
 import com.example.exa863_management_system_2023.model.ComputerComponent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class NewComponentController {
 
+    //Button
     @FXML
     private Button cancelButton;
-
-    @FXML
-    private TextField manufacturerField;
-
-    @FXML
-    private TextField nameField;
-
-    @FXML
-    private TextField quantityField;
-
     @FXML
     private Button saveButton;
 
+
+    //Field
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField manufacturerField;
     @FXML
     private TextField serialNumberField;
-
-    @FXML
-    private TextField unitCostField;
-
     @FXML
     private TextField unitPriceField;
+    @FXML
+    private TextField unitCostField;
+    @FXML
+    private TextField quantityField;
+
 
     @FXML
-    void cancelOperation(ActionEvent event) {
-
+    void cancelOperation(ActionEvent event) throws IOException {
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        MainController.gotoScene("StockView.fxml");
     }
 
     @FXML
-    void saveComponent(ActionEvent event) {
+    void saveComponent(ActionEvent event) throws IOException {
         double unitPrice = Double.parseDouble(unitPriceField.getText());
         double unitCost =  Double.parseDouble(unitCostField.getText());
         int quantity = Integer.parseInt(unitCostField.getText());
         ComputerComponent component = new ComputerComponent(nameField.getText(), manufacturerField.getText(), serialNumberField.getText(), unitPrice, unitCost, quantity);
         DAO.getComponent().create(component);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        MainController.gotoScene("StockView.fxml");
     }
 
 }
