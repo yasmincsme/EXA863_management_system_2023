38c4409e65ea5f89ee78355b4409d28a24c9b113
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-18 00:57:07 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/controller/UpdateClientController.java
+++ src/main/java/com/example/exa863_management_system_2023/controller/UpdateClientController.java
@@ -1,41 +1,64 @@
 package com.example.exa863_management_system_2023.controller;
 
+import com.example.exa863_management_system_2023.MainController;
+import com.example.exa863_management_system_2023.dao.DAO;
+import com.example.exa863_management_system_2023.exceptions.ObjectNotFoundException;
+import com.example.exa863_management_system_2023.model.Client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UpdateClientController {
 
-    @FXML
-    private TextField IDField;
-
-    @FXML
-    private TextField addressField;
-
+    //Button
     @FXML
     private Button cancelButton;
-
-    @FXML
-    private TextField emailField;
-
-    @FXML
-    private TextField nameField;
-
-    @FXML
-    private TextField phoneField;
-
     @FXML
     private Button saveButton;
 
-    @FXML
-    void cancelOperation(ActionEvent event) {
 
+    //Field
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField addressField;
+    @FXML
+    private TextField phoneField;
+
+
+    @FXML
+    void cancelOperation(ActionEvent event) throws IOException {
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        MainController.gotoScene("ClientView.fxml");
     }
 
     @FXML
-    void saveClient(ActionEvent event) {
+    void saveClient(ActionEvent event) throws ObjectNotFoundException, IOException {
+        Client client = (Client) ClientsController.selectedUser;
 
+        if (!nameField.getText().isEmpty()) {
+            client.setName(nameField.getText());
+        }
+        if (!emailField.getText().isEmpty()) {
+            client.setEmail(emailField.getText());
+        }
+        if (!addressField.getText().isEmpty()) {
+            client.setAddress(addressField.getText());
+        }
+        if (!phoneField.getText().isEmpty()) {
+            client.setPhone(phoneField.getText());
+        }
+        DAO.getClient().update(client);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        MainController.gotoScene("ClientView.fxml");
     }
-
 }
