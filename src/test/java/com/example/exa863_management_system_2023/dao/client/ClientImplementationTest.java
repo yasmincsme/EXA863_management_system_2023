087d7f858e2647941edd8d88382c379c74348a0d
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-18 01:06:31 -0300

    :recycle: refactor: Reorganizando classe

--- src/test/java/com/example/exa863_management_system_2023/dao/client/ClientImplementationTest.java
+++ src/test/java/com/example/exa863_management_system_2023/dao/client/ClientImplementationTest.java
@@ -1,6 +1,6 @@
 package com.example.exa863_management_system_2023.dao.client;
 
-import com.example.exa863_management_system_2023.Exceptions.ObjectNotFoundException;
+import com.example.exa863_management_system_2023.exceptions.ObjectNotFoundException;
 import com.example.exa863_management_system_2023.dao.DAO;
 import com.example.exa863_management_system_2023.model.Client;
 import org.junit.jupiter.api.*;
