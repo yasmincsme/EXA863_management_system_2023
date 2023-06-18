4b5e432adf0a775ddde0c2d807ba4c105e83ee57
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-18 00:59:16 -0300

    :sparkles: feat: Novo controller

--- src/main/java/com/example/exa863_management_system_2023/controller/NewClientController.java
+++ src/main/java/com/example/exa863_management_system_2023/controller/NewClientController.java
@@ -1,41 +1,51 @@
 package com.example.exa863_management_system_2023.controller;
 
+import com.example.exa863_management_system_2023.MainController;
 import com.example.exa863_management_system_2023.dao.DAO;
 import com.example.exa863_management_system_2023.model.Client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class NewClientController {
 
-    @FXML
-    private TextField addressField;
-
+    //Button
     @FXML
     private Button cancelButton;
-
-    @FXML
-    private TextField emailField;
-
-    @FXML
-    private TextField nameField;
-
-    @FXML
-    private TextField phoneField;
-
     @FXML
     private Button saveButton;
 
-    @FXML
-    void cancelOperation(ActionEvent event) {
 
+    //Field
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField addressField;
+    @FXML
+    private TextField phoneField;
+
+
+    @FXML
+    void cancelOperation(ActionEvent event) throws IOException {
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        MainController.gotoScene("ClientView.fxml");
     }
 
     @FXML
-    void saveClient(ActionEvent event) {
+    void saveClient(ActionEvent event) throws IOException {
         Client client = new Client(nameField.getText(), emailField.getText(), addressField.getText(), phoneField.getText());
         DAO.getClient().create(client);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        MainController.gotoScene("ClientView.fxml");
     }
 
 }
