81c8b618041618eb8f94800f309e70f6719c5bcd
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-06-18 01:06:43 -0300

    :recycle: refactor: Reorganizando classe

--- src/main/java/com/example/exa863_management_system_2023/dao/client/ClientListImplementation.java
+++ src/main/java/com/example/exa863_management_system_2023/dao/client/ClientListImplementation.java
@@ -1,6 +1,6 @@
 package com.example.exa863_management_system_2023.dao.client;
 
-import com.example.exa863_management_system_2023.Exceptions.ObjectNotFoundException;
+import com.example.exa863_management_system_2023.exceptions.ObjectNotFoundException;
 import com.example.exa863_management_system_2023.model.Client;
 import com.example.exa863_management_system_2023.utils.FileManager;
 import com.example.exa863_management_system_2023.utils.Generator;
